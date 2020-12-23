15b1a1b5ca0f5208a2d4c989ddcab8009b9d1bbb
Auto plugin <insprlabs@inspr.com>
2020-12-23 15:09:20 +0000

    [BOT] Adds test template for CORE-3

---
+++ terratests/CORE-3_test.go
@@ -0,0 +1,32 @@
+/*
+Template task for gleison
+
+h1. This is a task
+
+As a developer, I want to have a template task so that I know what to do on Jira
+*/
+package inspr_example
+import (
+        . "github.com/onsi/ginkgo"
+)
+Describe(" 1: is the context of the sprint", func() {
+Context(" a new sprint", func() {
+When(" that sprint starts" +
+"&& that sprint has tasks", func() {
+It(" the tasks layouts will match this", func() {
+
+})
+})
+})
+})
+
+Describe(" 2: is the context of the task", func() {
+Context(" a task", func() {
+When(" that task contains Functional Requirements" +
+"&& those requirements are in given when then format", func() {
+It(" the requirements will be translated to gingko testsdd", func() {
+
+})
+})
+})
+})
