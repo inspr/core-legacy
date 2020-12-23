1f629fa40b3b6f26d011a6b5aed141cefd839675
Auto plugin <insprlabs@inspr.com>
2020-12-23 13:34:46 +0000

    [AUTO] GOLANG COMMIT for testing

---
+++ terratests/Summary_ginkgo_test.go
@@ -0,0 +1,18 @@
+/*
+Tasks track small, distinct pieces of work.
+*/
+package inspr_example
+
+import (
+        . "github.com/onsi/ginkgo"
+)
+
+Describe(" Jira:", func() {
+Context(" a task", func() {
+When(" that task is added to a started sprint", func() {
+It(" the task will have it's tests automatically generated", func() {
+
+})
+})
+})
+})
