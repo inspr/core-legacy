6c91d71169c543101e20f5c2571ada5d8fecd222
Auto plugin <insprlabs@inspr.com>
2020-12-23 15:09:19 +0000

    [BOT] Adds test template for CORE-37

---
+++ terratests/CORE-37_test.go
@@ -0,0 +1,18 @@
+/*
+Summary
+
+This is a description for the task
+*/
+package inspr_example
+import (
+        . "github.com/onsi/ginkgo"
+)
+Describe(" Jira:", func() {
+Context(" a task", func() {
+When(" that task is added to a started sprint", func() {
+It(" the task will have it's tests automatically generated", func() {
+
+})
+})
+})
+})
