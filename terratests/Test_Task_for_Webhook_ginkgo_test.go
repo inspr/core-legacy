07dcd249d1791944a408ac07731ab32ca68112a1
Auto plugin <insprlabs@inspr.com>
2020-12-23 13:23:06 +0000

    [AUTO] GOLANG COMMIT for testing

---
+++ terratests/Test_Task_for_Webhook_ginkgo_test.go
@@ -0,0 +1,19 @@
+/*
+
+*/
+package inspr_example
+
+import (
+        . "github.com/onsi/ginkgo"
+)
+
+Describe(" Testing:", func() {
+Context(" a banana" +
+"&& a monkey", func() {
+When(" that monkey peels the banana", func() {
+It(" the banana can be eaten", func() {
+
+})
+})
+})
+})
