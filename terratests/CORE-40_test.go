d7b4f86cd96baaf5f302e9027344a7f492266e89
Auto plugin <insprlabs@inspr.com>
2021-01-13 18:10:43 +0000

    [BOT] Adds test template for CORE-40

---
+++ terratests/CORE-40_test.go
@@ -0,0 +1,10 @@
+/*
+DApps with managed lifecycles
+
+As a dApp developer, I would like to create applications whose nodes (sub-apps) and channels can be created and destroyed at runtime so that my application only consume resources when needed.
+*/
+package inspr_example
+
+import (
+	. "github.com/onsi/ginkgo"
+)
