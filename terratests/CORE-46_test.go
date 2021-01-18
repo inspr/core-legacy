1c0d41f7aa232e0575ac5354cfbc97fae05461c7
Auto plugin <insprlabs@inspr.com>
2021-01-18 18:39:44 +0000

    [BOT] Adds test template for CORE-46

---
+++ terratests/CORE-46_test.go
@@ -0,0 +1,27 @@
+/*
+Composable dApps
+
+As a dApp developer, I would like to take existing dApps and use them as a base to create my own dApp so that the solutions provided by Inspr's contributors are reused to their maximum and my application maintenance costs are lowered.
+
+When I’m building my dApp, I want to have an easy way to finish and compose my product, so that I spend less time working at the same dApp and start working on new projects.
+*/
+package inspr_example
+
+import (
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Context("*Scenario 1: When the developer wants to build a new project on Inspr*", func() {
+	Given("*Given* a new developer entering our platform", func() {
+		When("*When* that person has an idea of a solution", func() {
+			Then("*Then* it will be possible for her/him to build that solution connection apps that are already on Inspr HUB just by choosing what they want and connecting them", func() {
+			})
+		})
+	})
+	Given("*Given* that our dApp developer needs to upgrade its dApp", func() {
+		When("*When* the person finds a third party dApp that can help its solution", func() {
+			Then("*Then* the user can connect both dApps and launch a better version of the solution", func() {
+			})
+		})
+	})
+})
