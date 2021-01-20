75d9506f1177c0f9dab73c364f40ac3967b7668d
Nicholas Cristofaro <nicholaspcr@gmail.com>
2021-01-20 17:23:26 +0000

    handler base functions

--- cmd/insprd/api/controllers/routes.go
+++ cmd/insprd/api/controllers/routes.go
@@ -7,13 +7,23 @@ import (
 
 func (s *Server) initRoutes() {
 	s.Mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
 	})
 	s.Mux.HandleFunc("/app", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "App!")
 	})
+	// todo routes
+	// create/delete/update/get routes for channels.
+	// GET 		/channels 			->	returns all channels
+	// GET 		/channels/new		->	returns how to make a new channel
+	// POST		/channels 			->	creates a new channel
+	// GET		/channels/{ref}	->	returns info about one channel in ref
+	// PUT		/channels/{ref}	-> 	modifies the existing channel
+	// DELETE	/channels/{ref}	->	deletes a specific channel
 
-	s.Mux.HandleFunc("/cluster-state", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "funciton to return the cluster state!")
-	})
+	// create/delete/update/get routes for apps.
+	// same as channels
+
+	// create/delete/update/get routes for channel types.
+	// same as channels
+
 }
