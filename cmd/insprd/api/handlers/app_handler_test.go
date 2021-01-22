ebab083091cde0bff912939b3e35714a065b658f
Nicholas Cristofaro <nicholaspcr@gmail.com>
2021-01-22 18:23:59 +0000

    mocking server to tests handles, test

--- cmd/insprd/api/handlers/app_handler_test.go
+++ cmd/insprd/api/handlers/app_handler_test.go
@@ -1,11 +1,17 @@
 package handler
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 
 	"gitlab.inspr.dev/inspr/core/cmd/insprd/api/mocks"
+	"gitlab.inspr.dev/inspr/core/cmd/insprd/api/models"
 	"gitlab.inspr.dev/inspr/core/cmd/insprd/memory"
+	"gitlab.inspr.dev/inspr/core/pkg/meta"
 	"gitlab.inspr.dev/inspr/core/pkg/rest"
 )
 
@@ -55,21 +61,63 @@ func TestAppHandler_HandleCreateInfo(t *testing.T) {
 }
 
 func TestAppHandler_HandleCreateApp(t *testing.T) {
+	parsedAppDI, _ := json.Marshal(models.AppDI{
+		App: meta.App{},
+		Ctx: "",
+	})
 	tests := []struct {
 		name string
 		ah   *AppHandler
-		want rest.Handler
+		send struct {
+			reqBody []byte
+		}
+		want struct {
+			status int
+			err    error
+		}
 	}{
 		{
-			name: "t",
+			name: "successful_request",
 			ah:   NewAppHandler(mocks.MockMemoryManager(nil)),
-			want: nil,
+			send: struct{ reqBody []byte }{
+				reqBody: parsedAppDI,
+			},
+			want: struct {
+				status int
+				err    error
+			}{
+				status: http.StatusOK,
+				err:    nil,
+			},
+		},
+		{
+			name: "unsuccessful_request",
+			ah:   NewAppHandler(mocks.MockMemoryManager(nil)),
+			send: struct{ reqBody []byte }{
+				reqBody: parsedAppDI,
+			},
+			want: struct {
+				status int
+				err    error
+			}{
+				status: http.StatusOK,
+				err:    nil,
+			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.ah.HandleCreateApp(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("AppHandler.HandleCreateApp() = %v, want %v", got, tt.want)
+			handlerFunc := tt.ah.HandleCreateApp()
+			ts := httptest.NewServer(http.HandlerFunc((handlerFunc)))
+			defer ts.Close()
+			client := ts.Client()
+			res, err := client.Post(ts.URL, "application/json", bytes.NewBuffer(tt.send.reqBody))
+			if err != tt.want.err {
+				t.Errorf("AppHandler.HandleDeleteApp() = %v, want %v", err, tt.want.err)
+			}
+			defer res.Body.Close()
+			if res.StatusCode != tt.want.status {
+				t.Errorf("AppHandler.HandleDeleteApp() = %v, want %v", http.StatusOK, tt.want.status)
 			}
 		})
 	}
@@ -81,13 +129,14 @@ func TestAppHandler_HandleGetAppByRef(t *testing.T) {
 		ah   *AppHandler
 		want rest.Handler
 	}{
-		// TODO: Add test cases.
+		{
+			name: "working handler",
+			ah:   NewAppHandler(mocks.MockMemoryManager(nil)),
+			want: func(w http.ResponseWriter, r *http.Request) {},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.ah.HandleGetAppByRef(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("AppHandler.HandleGetAppByRef() = %v, want %v", got, tt.want)
-			}
 		})
 	}
 }
