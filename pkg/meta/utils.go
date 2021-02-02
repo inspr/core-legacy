1708a8cafcea9015b534776b41ee27349eaa4f89
Francisco Andrade <francisco@inspr.com>
2021-02-02 09:09:14 -0300

    Implemented regex for validanting structures' names

---
+++ pkg/meta/utils.go
@@ -0,0 +1,20 @@
+package meta
+
+import (
+	"regexp"
+
+	"gitlab.inspr.dev/inspr/core/pkg/ierrors"
+)
+
+// StructureNameIsValid checks if the given name is valid for naming Channels, CTypes and dApps
+func StructureNameIsValid(name string) (bool, error) {
+	if len(name) > 64 {
+		return false, ierrors.NewError().BadRequest().Message("name is too long").Build()
+	}
+	qnameCharFmt := "[A-Za-z0-9]"
+	qnameExtCharFmt := "[-A-Za-z0-9_]"
+	qualifiedNameFmt := "(" + qnameCharFmt + qnameExtCharFmt + "*)?" + qnameCharFmt
+	var r = regexp.MustCompile("^" + qualifiedNameFmt + "$")
+
+	return r.MatchString(name), nil
+}
