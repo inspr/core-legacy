d75359564edd4623ee3012673ac495d28223a0a6
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2021-02-03 12:08:30 -0300

    Fix: DCopy lint

--- pkg/utils/deep_copy.go
+++ pkg/utils/deep_copy.go
@@ -6,6 +6,7 @@ import (
 	"gitlab.inspr.dev/inspr/core/pkg/meta"
 )
 
+//DCopy is a copy method for dapps tree structures
 func DCopy(root *meta.App) *meta.App {
 	rootObj, _ := json.Marshal(*root)
 	temp := meta.App{}
