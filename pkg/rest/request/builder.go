83718544f43b44feec97d00eea80394e883d924e
Pedro <ptcar@inspr.com>
2021-02-05 17:19:39 -0300

    feat: client builder for requests

---
+++ pkg/rest/request/builder.go
@@ -0,0 +1,44 @@
+package request
+
+import "net/http"
+
+// ClientBuilder builds a client with the given specifications
+type ClientBuilder struct {
+	c *Client
+}
+
+// BaseURL sets the base URL for the client that is being built
+func (cb *ClientBuilder) BaseURL(url string) *ClientBuilder {
+	cb.c.baseURL = url
+	return cb
+}
+
+// Encoder sets the encoder for the client that is being built
+func (cb *ClientBuilder) Encoder(encoder Encoder) *ClientBuilder {
+	cb.c.encoder = encoder
+	return cb
+}
+
+// Decoder sets the decoder for the client that is being built
+func (cb *ClientBuilder) Decoder(decoder DecoderGenerator) *ClientBuilder {
+	cb.c.decoderGenerator = decoder
+	return cb
+}
+
+// HTTPClient sets the http client for the client that is being built
+func (cb *ClientBuilder) HTTPClient(client http.Client) *ClientBuilder {
+	cb.c.c = client
+	return cb
+}
+
+// NewClient creates a builder for a client
+func NewClient() *ClientBuilder {
+	return &ClientBuilder{
+		c: &Client{},
+	}
+}
+
+// Build returns the client built by the builder
+func (cb *ClientBuilder) Build() *Client {
+	return cb.c
+}
