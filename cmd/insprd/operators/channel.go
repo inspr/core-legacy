dce328c7527b711504a846c4ff139f9bf701f37a
Pedro <ptcar@inspr.com>
2021-02-09 15:29:07 -0300

    fix: change interface to allow context

--- cmd/insprd/operators/channel.go
+++ cmd/insprd/operators/channel.go
@@ -1,6 +1,10 @@
 package operators
 
-import "gitlab.inspr.dev/inspr/core/pkg/meta"
+import (
+	"context"
+
+	"gitlab.inspr.dev/inspr/core/pkg/meta"
+)
 
 // ChannelOperatorInterface is responsible for handling the following methods
 //
@@ -10,11 +14,11 @@ import "gitlab.inspr.dev/inspr/core/pkg/meta"
 // 	- `Update`: updates a channel in the DApp of the given context
 // 	- `Delete`: deletes a channel of the specified name in DApp of the given context
 type ChannelOperatorInterface interface {
-	Get(ctx string, name string) (*meta.Channel, error)
-	GetAll(ctx string) ([]*meta.Channel, error)
-	Create(ctx string, channel *meta.Channel) error
-	Update(ctx string, channel *meta.Channel) error
-	Delete(ctx string, name string) error
+	Get(ctx context.Context, context string, name string) (*meta.Channel, error)
+	GetAll(ctx context.Context, context string) ([]*meta.Channel, error)
+	Create(ctx context.Context, context string, channel *meta.Channel) error
+	Update(ctx context.Context, context string, channel *meta.Channel) error
+	Delete(ctx context.Context, context string, name string) error
 }
 
 // ChannelOperator is the struct that has the elements
@@ -28,28 +32,28 @@ func NewChannelOperator() *ChannelOperator {
 }
 
 // Get - returns a channel given the search context and it's name
-func (co *ChannelOperator) Get(ctx string, name string) (*meta.Channel, error) {
+func (co *ChannelOperator) Get(ctx context.Context, context string, name string) (*meta.Channel, error) {
 	return &meta.Channel{}, nil
 }
 
 // GetAll - returns all channels in the given context
-func (co *ChannelOperator) GetAll(ctx string) ([]*meta.Channel, error) {
+func (co *ChannelOperator) GetAll(ctx context.Context, context string) ([]*meta.Channel, error) {
 	return []*meta.Channel{{}, {}}, nil
 }
 
 // Create - adds the channel passed in the parameter into the context passed
 // before doing so some tests are made to be sure no conflicts happen
-func (co *ChannelOperator) Create(ctx string, channel *meta.Channel) error {
+func (co *ChannelOperator) Create(ctx context.Context, context string, channel *meta.Channel) error {
 	return nil
 }
 
 // Update - updates the channel in the context given, the way it identifies
 // which channel to change is through channel.Meta.Name
-func (co *ChannelOperator) Update(ctx string, channel *meta.Channel) error {
+func (co *ChannelOperator) Update(ctx context.Context, context string, channel *meta.Channel) error {
 	return nil
 }
 
 // Delete - deletes the channel in the context given and with the name specified
-func (co *ChannelOperator) Delete(ctx string, name string) error {
+func (co *ChannelOperator) Delete(ctx context.Context, context string, name string) error {
 	return nil
 }
