b6c275ba251891a310160917cf526a28d12c2cf2
luiz-couto <luiz.felipe.556@outlook.com>
2021-02-12 12:31:47 -0300

    test: more integration tests

--- pkg/controller/test/main.go
+++ pkg/controller/test/main.go
@@ -8,7 +8,6 @@ import (
 	"gitlab.inspr.dev/inspr/core/pkg/controller/client"
 	"gitlab.inspr.dev/inspr/core/pkg/meta"
 	"gitlab.inspr.dev/inspr/core/pkg/rest/request"
-	"gitlab.inspr.dev/inspr/core/pkg/utils/diff"
 )
 
 func main() {
@@ -17,48 +16,48 @@ func main() {
 	}
 
 	fmt.Println("[Creating App HelloWorld in Root...]")
-	resp, _ := createHelloWorldApp(&client)
-	resp.Print()
+	createHelloWorldApp(&client)
 	fmt.Printf("\n\n")
 
 	fmt.Println("[Trying to create HelloWorld again...]")
-	_, err := createHelloWorldApp(&client)
-	fmt.Println(err)
+	createHelloWorldApp(&client)
 	fmt.Printf("\n\n")
 
-	fmt.Println("[Trying to create ChannelOne inside HelloWorld app...]")
-	resp, _ = createChannelInsideHelloWorld(&client)
-	resp.Print()
+	fmt.Println("[Creating ChannelTypeHello inside HelloWorld app...]")
+	createChannelTypeInsideHelloWorld(&client)
+	fmt.Printf("\n\n")
+
+	fmt.Println("[Creating ChannelOne inside HelloWorld app...]")
+	createChannelInsideHelloWorld(&client)
+	fmt.Printf("\n\n")
+
+	fmt.Println("[Creating NewApp inside HelloWorld app...]")
+	createNewAppInsideHelloWorld(&client)
+	fmt.Printf("\n\n")
+
+	fmt.Println("[Update NewApp adding a new boundary: ChannelOne as Input...]")
+	updateNewAppAddBoundary(&client)
 	fmt.Printf("\n\n")
 
 }
 
-func createHelloWorldApp(client *client.Client) (diff.Changelog, error) {
+func createHelloWorldApp(client *client.Client) {
 	resp, err := client.Apps().Create(context.Background(), "", &meta.App{
 		Meta: meta.Metadata{
 			Name: "HelloWorld",
 		},
-		Spec: meta.AppSpec{
-			ChannelTypes: map[string]*meta.ChannelType{
-				"ChannelTypeHello": {
-					Meta: meta.Metadata{
-						Name: "ChannelTypeHello",
-					},
-				},
-			},
-		},
+		Spec: meta.AppSpec{},
 	})
 	if err != nil {
 		fmt.Printf("%#v", err)
-		return nil, err
 	}
-	return resp, nil
+	resp.Print()
 }
 
-func createChannelInsideHelloWorld(client *client.Client) (diff.Changelog, error) {
+func createChannelInsideHelloWorld(client *client.Client) {
 	resp, err := client.Channels().Create(context.Background(), "HelloWorld", &meta.Channel{
 		Meta: meta.Metadata{
-			Name: "channelOne",
+			Name: "ChannelOne",
 		},
 		Spec: meta.ChannelSpec{
 			Type: "ChannelTypeHello",
@@ -66,7 +65,48 @@ func createChannelInsideHelloWorld(client *client.Client) (diff.Changelog, error
 	})
 	if err != nil {
 		fmt.Printf("%#v", err)
-		return nil, err
 	}
-	return resp, nil
+	resp.Print()
+}
+
+func createChannelTypeInsideHelloWorld(client *client.Client) {
+	resp, err := client.ChannelTypes().Create(context.Background(), "HelloWorld", &meta.ChannelType{
+		Meta: meta.Metadata{
+			Name: "ChannelTypeHello",
+		},
+	})
+	if err != nil {
+		fmt.Printf("%#v", err)
+	}
+	resp.Print()
+}
+
+func createNewAppInsideHelloWorld(client *client.Client) {
+	resp, err := client.Apps().Create(context.Background(), "HelloWorld", &meta.App{
+		Meta: meta.Metadata{
+			Name: "NewApp",
+		},
+		Spec: meta.AppSpec{},
+	})
+	if err != nil {
+		fmt.Printf("%#v", err)
+	}
+	resp.Print()
+}
+
+func updateNewAppAddBoundary(client *client.Client) {
+	resp, err := client.Apps().Update(context.Background(), "HelloWorld.NewApp", &meta.App{
+		Meta: meta.Metadata{
+			Name: "NewApp",
+		},
+		Spec: meta.AppSpec{
+			Boundary: meta.AppBoundary{
+				Input: []string{"ChannelOne"},
+			},
+		},
+	})
+	if err != nil {
+		fmt.Printf("%#v", err)
+	}
+	resp.Print()
 }
