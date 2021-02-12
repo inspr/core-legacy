337c43d3040a57d343e2354a8aafa8cb39ddb6d0
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2021-02-12 13:47:33 -0300

    Dev: node operatoritions

--- cmd/insprd/operators/kafka/nodes/node_operator.go
+++ cmd/insprd/operators/kafka/nodes/node_operator.go
@@ -6,7 +6,9 @@ import (
 	"strings"
 
 	"gitlab.inspr.dev/inspr/core/cmd/insprd/operators"
-	"gitlab.inspr.dev/inspr/core/pkg/enviroment"
+	operator "gitlab.inspr.dev/inspr/core/pkg/client/k8s-op"
+	"gitlab.inspr.dev/inspr/core/pkg/environment"
+	"gitlab.inspr.dev/inspr/core/pkg/ierrors"
 	"gitlab.inspr.dev/inspr/core/pkg/meta"
 
 	kubeApp "k8s.io/api/apps/v1"
@@ -15,6 +17,7 @@ import (
 	v1 "k8s.io/client-go/kubernetes/typed/apps/v1"
 )
 
+//NodeOperator defines a node operations interface.
 type NodeOperator struct {
 	clientSet kubernetes.Interface
 }
@@ -28,11 +31,17 @@ func (nop *NodeOperator) retrieveKube() v1.DeploymentInterface {
 // Otherwise, returns an error
 func (nop *NodeOperator) GetNode(ctx context.Context, app *meta.App) (*meta.Node, error) {
 	kube := nop.retrieveKube()
-	insprEnv := enviroment.GetEnvironment().InsprEnvironment
+	insprEnv := environment.GetEnvironment().InsprEnvironment
 	nodeName := parseNodeName(insprEnv, app.Meta.Parent, app.Spec.Node.Meta.Name)
-	_, err := kube.Get(nodeName, metav1.GetOptions{})
-	//
-	return &app.Spec.Node, err
+	dep, err := kube.Get(nodeName, metav1.GetOptions{})
+	if err != nil {
+		return &meta.Node{}, ierrors.NewError().Message("could't get deployment from kubernetes").InnerError(err).Build()
+	}
+	node, err := operator.ToNode(dep)
+	if err != nil {
+		return &meta.Node{}, err
+	}
+	return node, nil
 }
 
 // Nodes is a NodeOperatorInterface that provides methods for node manipulation
@@ -42,32 +51,58 @@ func (nop *NodeOperator) Nodes() operators.NodeOperatorInterface {
 
 // CreateNode deploys a new node structure, if it's information is valid.
 // Otherwise, returns an error
-func (nop *NodeOperator) CreateNode(ctx context.Context, app *meta.App) error {
-	return nil
+func (nop *NodeOperator) CreateNode(ctx context.Context, app *meta.App) (*meta.Node, error) {
+	var deploy *kubeApp.Deployment
+	kube := nop.retrieveKube()
+	deploy = operator.InsprDAppToK8sDeployment(app)
+	dep, err := kube.Create(deploy)
+	if err != nil {
+		return &meta.Node{}, ierrors.NewError().Message("could't create deployment from kubernetes").InnerError(err).Build()
+	}
+	node, err := operator.ToNode(dep)
+	if err != nil {
+		return &meta.Node{}, err
+	}
+	return node, nil
 }
 
 // UpdateNode updates a node that already exists, if the new structure is valid.
 // Otherwise, returns an error.
-func (nop *NodeOperator) UpdateNode(ctx context.Context, app *meta.App) error {
+func (nop *NodeOperator) UpdateNode(ctx context.Context, app *meta.App) (*meta.Node, error) {
 	var deploy *kubeApp.Deployment
 	kube := nop.retrieveKube()
-	// deploy = translatenodetodeploy
-	_, err := kube.Update(deploy)
-
-	return err
+	deploy = operator.InsprDAppToK8sDeployment(app)
+	dep, err := kube.Update(deploy)
+	if err != nil {
+		return &meta.Node{}, ierrors.NewError().Message("could't update deployment from kubernetes").InnerError(err).Build()
+	}
+	node, err := operator.ToNode(dep)
+	if err != nil {
+		return &meta.Node{}, err
+	}
+	return node, nil
 }
 
 // DeleteNode deletes node with given name, if it exists. Otherwise, returns an error
 func (nop *NodeOperator) DeleteNode(ctx context.Context, nodeContext string, nodeName string) error {
-	return nil
+	var deploy string
+	kube := nop.retrieveKube()
+	deploy = parseNodeName(GetK8SVariables().AppsNamespace, nodeContext, nodeName)
+	err := kube.Delete(deploy, &metav1.DeleteOptions{})
+
+	return ierrors.NewError().Message("could't delete deployment from kubernetes").InnerError(err).Build()
 }
 
 // GetAllNodes returns a list of all the active nodes in the deployment, if there are any
 func (nop *NodeOperator) GetAllNodes() []*meta.Node {
+	var nodes []*meta.Node
 	kube := nop.retrieveKube()
-	kube.List(metav1.ListOptions{})
-	return nil
-
+	list, _ := kube.List(metav1.ListOptions{})
+	for _, item := range list.Items {
+		node, _ := operator.ToNode(&item)
+		nodes = append(nodes, node)
+	}
+	return nodes
 }
 
 func parseNodeName(insprEnv string, context string, name string) string {
--- cmd/insprd/operators/node.go
+++ cmd/insprd/operators/node.go
@@ -9,9 +9,9 @@ import (
 // NodeOperatorInterface is the interface that allows to obtain or change
 // node information inside a deployment
 type NodeOperatorInterface interface {
-	CreateNode(ctx context.Context, app *meta.App) error
+	CreateNode(ctx context.Context, app *meta.App) (*meta.Node, error)
 	GetNode(ctx context.Context, app *meta.App) (*meta.Node, error)
-	UpdateNode(ctx context.Context, app *meta.App) error
+	UpdateNode(ctx context.Context, app *meta.App) (*meta.Node, error)
 	DeleteNode(ctx context.Context, nodeContext string, nodeName string) error
 	GetAllNodes() []*meta.Node
 }
