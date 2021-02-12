e00312023f4dafb082e4fb7f04ee31746e76956f
Francisco Andrade <francisco@inspr.com>
2021-02-12 12:19:35 -0300

    Dev: added error treatment to sidecars/kafka main

--- cmd/sidecars/kafka/main.go
+++ cmd/sidecars/kafka/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	kafkasc "gitlab.inspr.dev/inspr/core/cmd/sidecars/kafka/client"
 	sidecarserv "gitlab.inspr.dev/inspr/core/pkg/sidecar/server"
@@ -11,12 +12,14 @@ func main() {
 	ctx := context.Background()
 	reader, err := kafkasc.NewReader()
 	if err != nil {
-
+		fmt.Println(err)
+		return
 	}
 
 	writer, err := kafkasc.NewWriter(false)
 	if err != nil {
-
+		fmt.Println(err)
+		return
 	}
 
 	s := sidecarserv.NewServer()
