e8d98c40344583b2fd69995c4fb3c92fcf614e71
Francisco Andrade <francisco@inspr.com>
2021-02-12 12:17:06 -0300

    Refactor: removed unutilized structure in sidecar models

--- pkg/sidecar/models/types.go
+++ pkg/sidecar/models/types.go
@@ -1,15 +1,5 @@
 package models
 
-// RECEIVED BY BROKER's INTERFACE METHODS
-
-// BrokerResponse - is the struct that represents
-// the return of the interface methods
-type BrokerResponse struct {
-	Data interface{} `json:"data,omitempty"`
-}
-
-// RECEIVED BY DAPP CLIENT
-
 // Message - data to be put in the message
 type Message struct {
 	Data interface{} `json:"data,omitempty"`
