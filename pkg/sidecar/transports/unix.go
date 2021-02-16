20ab2034b87ef4b63db2ef337e07b7214bdb0162
Pedro <ptcar@inspr.com>
2021-02-16 18:39:59 -0300

    fix: unix socket injection

--- pkg/sidecar/transports/unix.go
+++ pkg/sidecar/transports/unix.go
@@ -7,11 +7,10 @@ import (
 
 // NewUnixSocketClient returna um client que conecta via unix socket
 func NewUnixSocketClient(addr string) http.Client {
-	sockAddr := "/inspr/" + addr + ".sock"
 	return http.Client{
 		Transport: &http.Transport{
 			Dial: func(string, string) (net.Conn, error) {
-				return net.Dial("unix", sockAddr)
+				return net.Dial("unix", addr)
 			},
 		},
 	}
