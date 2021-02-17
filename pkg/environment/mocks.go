f4891f93846bca3f697651b0683fe72bedd3e63b
Pedro <ptcar@inspr.com>
2021-02-17 09:52:49 -0300

    fix: changes mock to be like join

--- pkg/environment/mocks.go
+++ pkg/environment/mocks.go
@@ -4,12 +4,14 @@ import "os"
 
 // SetMockEnv - sets the environment variables to specific values
 func SetMockEnv() {
-	channelsValues := "chan;chan1;chan2;chan3;"
+	channelsValues := "chan;chan1;chan2;chan3"
 	os.Setenv("INSPR_INPUT_CHANNELS", channelsValues)
 	os.Setenv("INSPR_OUTPUT_CHANNELS", channelsValues)
 	os.Setenv("INSPR_UNIX_SOCKET", "socket_addr")
 	os.Setenv("INSPR_SIDECAR_IMAGE", "mock_sidecar_image")
+
 	os.Setenv("INSPR_APP_CTX", "mock.dapp.context")
+
 	os.Setenv("INSPR_ENV", "mock_env")
 	os.Setenv("INSPR_APP_ID", "testappid1")
 	os.Setenv("INSPR_SIDECAR_IMAGE", "random-sidecar-image")
