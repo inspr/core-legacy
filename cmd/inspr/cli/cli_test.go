e43a2497b10ed4136e0ab2fc1089462800222b65
nicholas cristofaro <nick@pop-os.localdomain>
2021-02-17 13:30:45 -0300

    adding tests

---
+++ cmd/inspr/cli/cli_test.go
@@ -0,0 +1,35 @@
+package cli
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+
+	"github.com/spf13/cobra"
+)
+
+func TestNewInsprCommand(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    *cobra.Command
+		wantOut string
+		wantErr string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			out := &bytes.Buffer{}
+			err := &bytes.Buffer{}
+			if got := NewInsprCommand(out, err); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewInsprCommand() = %v, want %v", got, tt.want)
+			}
+			if gotOut := out.String(); gotOut != tt.wantOut {
+				t.Errorf("NewInsprCommand() = %v, want %v", gotOut, tt.wantOut)
+			}
+			if gotErr := err.String(); gotErr != tt.wantErr {
+				t.Errorf("NewInsprCommand() = %v, want %v", gotErr, tt.wantErr)
+			}
+		})
+	}
+}
