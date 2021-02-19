9e3d257ce81b6484d5fa84871f127b28261478a2
luiz-couto <luiz.felipe.556@outlook.com>
2021-02-19 14:39:32 -0300

    created factory and methods

---
+++ cmd/inspr/cli/apply_factory.go
@@ -0,0 +1,53 @@
+package cli
+
+import (
+	"github.com/spf13/cobra"
+	"gitlab.inspr.dev/inspr/core/pkg/meta"
+)
+
+/*
+RunMethod defines the method that will run for the
+component
+*/
+type RunMethod func(*cobra.Command, []string)
+
+/*
+ApplyFactory holds a dictionary that maps all the pairs
+(kind, apiVersion - encapsulated in the Component Type)
+to a cobra run method
+*/
+type ApplyFactory struct {
+	applyDict map[meta.Component]RunMethod
+}
+
+var applyFactory *ApplyFactory
+
+/*
+GetFactory returns the ApllyFactory singleton.
+If it doesn't exist, create one
+*/
+func GetFactory() *ApplyFactory {
+	if applyFactory == nil {
+		applyFactory = &ApplyFactory{
+			applyDict: make(map[meta.Component]RunMethod),
+		}
+	}
+	return applyFactory
+}
+
+/*
+GetRunMethod returns the runMethod registered for the
+given component
+*/
+func (af *ApplyFactory) GetRunMethod(component meta.Component) RunMethod {
+	return af.applyDict[component]
+}
+
+/*
+Subscribe adds to the apply factory dictonary the
+given component with the value equals to the given
+runMethod
+*/
+func (af *ApplyFactory) Subscribe(component meta.Component, method RunMethod) {
+	af.applyDict[component] = method
+}
