99d7883deb88a98fd16d9a0863dcc67791bcc76a
Francisco Andrade <francisco@inspr.com>
2021-02-23 10:12:13 -0300

    Test: improved tests for apply command

--- cmd/inspr/cli/apply_test.go
+++ cmd/inspr/cli/apply_test.go
@@ -2,27 +2,28 @@ package cli
 
 import (
 	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"reflect"
 	"testing"
 
-	"github.com/spf13/cobra"
 	"gitlab.inspr.dev/inspr/core/pkg/meta"
 	"gopkg.in/yaml.v2"
 )
 
-func createYaml() (string, meta.Channel) {
-	channel := meta.Channel{
-		Meta: meta.Metadata{
-			Name:        "mock_name",
-			Reference:   "mock_reference",
-			Annotations: map[string]string{},
-			Parent:      "mock_parent",
-			SHA256:      "mock_sha256",
-		},
-		Spec:          meta.ChannelSpec{Type: "mock_type"},
-		ConnectedApps: []string{"a", "b", "c"},
+const (
+	filePath = "filetest.yaml"
+)
+
+func createYaml() string {
+	comp := meta.Component{
+		Kind:       "app",
+		APIVersion: "v1",
 	}
-	data, _ := yaml.Marshal(&channel)
-	return string(data), channel
+	data, _ := yaml.Marshal(&comp)
+	return string(data)
 }
 
 // TestNewApplyCmd is mainly for improving test coverage,
@@ -30,18 +31,16 @@ func createYaml() (string, meta.Channel) {
 func TestNewApplyCmd(t *testing.T) {
 	tests := []struct {
 		name string
-		want *cobra.Command
 	}{
 		{
 			name: "Creates a new Cobra command",
-			want: &cobra.Command{},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := NewApplyCmd()
 			if got == nil {
-				t.Errorf("NewApplyCmd() = %v, want %v", got, tt.want)
+				t.Errorf("NewApplyCmd() = %v", got)
 			}
 		})
 	}
@@ -119,4 +118,132 @@ func Test_printAppliedFiles(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_doApply(t *testing.T) {
+	type args struct {
+		in0 context.Context
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantOut string
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			out := &bytes.Buffer{}
+			if err := doApply(tt.args.in0, out); (err != nil) != tt.wantErr {
+				t.Errorf("doApply() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotOut := out.String(); gotOut != tt.wantOut {
+				t.Errorf("doApply() = %v, want %v", gotOut, tt.wantOut)
+			}
+		})
+	}
+}
+
+func Test_getFilesFromFolder(t *testing.T) {
+	tempFiles := []string{}
+	yamlString := createYaml()
+	// creates a file with the expected syntax
+	ioutil.WriteFile(
+		filePath,
+		[]byte(yamlString),
+		os.ModePerm,
+	)
+
+	type args struct {
+		path  string
+		files *[]string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Get file from current folder",
+			args: args{
+				path:  ".",
+				files: &tempFiles,
+			},
+			wantErr: false,
+		},
+		{
+			name: "Invalid - path doesn't exist",
+			args: args{
+				path:  "invalid/",
+				files: &tempFiles,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := getFilesFromFolder(tt.args.path, tt.args.files); (err != nil) != tt.wantErr {
+				t.Errorf("getFilesFromFolder() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+	os.Remove(filePath)
+}
+
+func Test_applyValidFiles(t *testing.T) {
+	tempFiles := []string{filePath}
+	yamlString := createYaml()
+	// creates a file with the expected syntax
+	ioutil.WriteFile(
+		filePath,
+		[]byte(yamlString),
+		os.ModePerm,
+	)
+
+	type args struct {
+		path  string
+		files []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []applied
+	}{
+		{
+			name: "Get file from current folder",
+			args: args{
+				path:  "",
+				files: tempFiles,
+			},
+			want: []applied{{
+				file: filePath,
+				component: meta.Component{
+					Kind:       "app",
+					APIVersion: "v1",
+				},
+			}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			GetFactory().Subscribe(meta.Component{
+				APIVersion: "v1",
+				Kind:       "app",
+			},
+				func(b []byte) error {
+					ch := meta.Channel{}
+
+					yaml.Unmarshal(b, &ch)
+					fmt.Println(ch)
+
+					return nil
+				})
+			if got := applyValidFiles(tt.args.path, tt.args.files); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("applyValidFiles() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+	os.Remove(filePath)
 }
