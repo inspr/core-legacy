c2456c16c5d8c99d7030b8ff41b237351376d767
nicholas cristofaro <nicholas@inspr.com>
2021-02-24 11:31:53 -0300

    removing unecessary comments

--- cmd/inspr/cli/apply_channel.go
+++ cmd/inspr/cli/apply_channel.go
@@ -10,12 +10,6 @@ import (
 	utils "gitlab.inspr.dev/inspr/core/pkg/meta/utils/parser"
 )
 
-// RunMethod defines the method that will run for the component
-// type RunMethod func(data []byte, out io.Writer) error
-
-// ApplyChannel is of the type RunMethod, it calls the pkg/controller/client functions.
-type ApplyChannel RunMethod
-
 // NewApplyChannel receives a controller ChannelInterface and calls it's methods
 // depending on the flags values
 func NewApplyChannel(c controller.ChannelInterface) RunMethod {
