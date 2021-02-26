aae1ef7447bb2eda7d168c56962ba24ef23ca191
Pedro <ptcar@inspr.com>
2021-02-26 11:34:05 -0300

    feat: changes memory interface to have a get interface

--- cmd/insprd/memory/interface.go
+++ cmd/insprd/memory/interface.go
@@ -12,10 +12,13 @@ import (
 // the Channels in the cluster
 type ChannelMemory interface {
 	TransactionInterface
-	GetChannel(context string, chName string) (*meta.Channel, error)
+	ChannelGetInterface
 	CreateChannel(context string, ch *meta.Channel) error
 	DeleteChannel(context string, chName string) error
 	UpdateChannel(context string, ch *meta.Channel) error
+}
+type ChannelGetInterface interface {
+	Get(context string, ctName string) (*meta.Channel, error)
 }
 
 // AppMemory is the interface that allows to obtain or
@@ -23,10 +26,13 @@ type ChannelMemory interface {
 // the DApps in the cluster
 type AppMemory interface {
 	TransactionInterface
-	GetApp(query string) (*meta.App, error)
+	AppGetInterface
 	CreateApp(app *meta.App, context string) error
 	DeleteApp(query string) error
 	UpdateApp(app *meta.App, query string) error
+}
+type AppGetInterface interface {
+	Get(query string) (*meta.App, error)
 }
 
 // ChannelTypeMemory is the interface that allows to
@@ -34,10 +40,13 @@ type AppMemory interface {
 // state of the ChannelTypes in the cluster
 type ChannelTypeMemory interface {
 	TransactionInterface
-	GetChannelType(context string, ctName string) (*meta.ChannelType, error)
+	ChannelTypeGetInterface
 	CreateChannelType(ct *meta.ChannelType, context string) error
 	DeleteChannelType(context string, ctName string) error
 	UpdateChannelType(ct *meta.ChannelType, context string) error
+}
+type ChannelTypeGetInterface interface {
+	Get(context string, ctName string) (*meta.ChannelType, error)
 }
 
 // Manager is the interface that allows the management
@@ -48,6 +57,12 @@ type Manager interface {
 	Apps() AppMemory
 	Channels() ChannelMemory
 	ChannelTypes() ChannelTypeMemory
+}
+
+type GetInterface interface {
+	Apps() AppGetInterface
+	Channels() ChannelGetInterface
+	ChannelTypes() ChannelTypeGetInterface
 }
 
 // TransactionInterface makes transactions on a Memory manager
