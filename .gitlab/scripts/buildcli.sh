f2de3e9ac2db81e27f1e06e8a816e53955c8198a
Francisco Andrade <francisco@inspr.com>
2021-03-05 10:01:16 -0300

    Dev: changed 'deploy' to 'release' and added windows 386 support

--- .gitlab/scripts/buildcli.sh
+++ .gitlab/scripts/buildcli.sh
@@ -7,8 +7,9 @@ GOOS=linux GOARCH=arm64 go build -ldflags "-X cmd.inspr.version=$VERSION" -o ./b
 GOOS=linux GOARCH=amd64 go build -ldflags "-X cmd.inspr.version=$VERSION" -o ./bin/insprcli-linux-amd64-$VERSION ./cmd/inspr
 
 echo "Building CLI for Windows"
-GOOS=windows GOARCH=amd64 go build -ldflags "-X cmd.inspr.version=$VERSION" -o ./bin/insprcli-windows-amd64-$VERSION ./cmd/inspr 
+GOOS=windows GOARCH=amd64 go build -ldflags "-X cmd.inspr.version=$VERSION" -o ./bin/insprcli-windows-amd64-$VERSION ./cmd/inspr
+GOOS=windows GOARCH=386 go build -ldflags "-X cmd.inspr.version=$VERSION" -o ./bin/insprcli-windows-386-$VERSION ./cmd/inspr
 
 echo "Building CLI for Mac"
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-X cmd.inspr.version=$VERSION" -o ./bin/insprcli-darwin-amd64-$VERSION ./cmd/inspr 
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-X cmd.inspr.version=$VERSION" -o ./bin/insprcli-darwin-amd64-$VERSION ./cmd/inspr
 
