856f5fc64a4b06524f00d83338be8b2abe7c2119
Francisco Andrade <francisco@inspr.com>
2021-03-05 11:47:12 -0300

    Fix: created BASE_URL var in releasecli.sh

--- .gitlab/scripts/releasecli.sh
+++ .gitlab/scripts/releasecli.sh
@@ -1,4 +1,5 @@
 VERSION=$(git describe --always)
+BASE_URL="https://console.cloud.google.com/storage/browser/inspr-cli/bin"
 
 gsutil rsync bin gs://inspr-cli/$VERSION
 
@@ -6,12 +7,12 @@ release-cli create --name "Release $VERSION" \
         --tag-name $VERSION \
         --ref "$CI_COMMIT_SHA" \
         --assets-link "{\"name\":\"insprcli-linux-arm64\",
-                        \"url\":\"https://console.cloud.google.com/storage/browser/inspr-cli/bin/insprcli-linux-arm64-$VERSION\"}" \
+                        \"url\":\"$BASE_URL/insprcli-linux-arm64-$VERSION\"}" \
         --assets-link "{\"name\":\"insprcli-linux-amd64\",
-                        \"url\":\"https://console.cloud.google.com/storage/browser/inspr-cli/bin/insprcli-linux-amd64-$VERSION\"}" \
+                        \"url\":\"$BASE_URL/bin/insprcli-linux-amd64-$VERSION\"}" \
         --assets-link "{\"name\":\"insprcli-windows-amd64\",
-                        \"url\":\"https://console.cloud.google.com/storage/browser/inspr-cli/bin/insprcli-windows-amd64-$VERSION\"}" \
+                        \"url\":\"$BASE_URL/bin/insprcli-windows-amd64-$VERSION\"}" \
         --assets-link "{\"name\":\"insprcli-windows-386\",
-                        \"url\":\"https://console.cloud.google.com/storage/browser/inspr-cli/bin/insprcli-windows-386-$VERSION\"}" \
+                        \"url\":\"$BASE_URL/bin/insprcli-windows-386-$VERSION\"}" \
         --assets-link "{\"name\":\"insprcli-darwin-amd64\",
-                        \"url\":\"https://console.cloud.google.com/storage/browser/inspr-cli/bin/insprcli-darwin-amd64-$VERSION\"}"
+                        \"url\":\"$BASE_URL/bin/insprcli-darwin-amd64-$VERSION\"}"
