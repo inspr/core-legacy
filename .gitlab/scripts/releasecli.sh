2d27328e107b3e9fc3d0ede3ad6baa64ac491cbb
Francisco Andrade <francisco@inspr.com>
2021-03-05 11:11:37 -0300

    Fix: removed newline between commands in releasecli

--- .gitlab/scripts/releasecli.sh
+++ .gitlab/scripts/releasecli.sh
@@ -7,15 +7,11 @@ release-cli create --name "Release $VERSION" \
         --ref "$CI_COMMIT_SHA" \
         --assets-link "{\"name\":\"insprcli-linux-arm64\",
                         \"url\":\"https://console.cloud.google.com/storage/browser/inspr-cli/bin/insprcli-linux-arm64-$VERSION\"}" \
-
         --assets-link "{\"name\":\"insprcli-linux-amd64\",
                         \"url\":\"https://console.cloud.google.com/storage/browser/inspr-cli/bin/insprcli-linux-amd64-$VERSION\"}" \
-
         --assets-link "{\"name\":\"insprcli-windows-amd64\",
                         \"url\":\"https://console.cloud.google.com/storage/browser/inspr-cli/bin/insprcli-windows-amd64-$VERSION\"}" \
-
         --assets-link "{\"name\":\"insprcli-windows-386\",
                         \"url\":\"https://console.cloud.google.com/storage/browser/inspr-cli/bin/insprcli-windows-386-$VERSION\"}" \
-                        
         --assets-link "{\"name\":\"insprcli-darwin-amd64\",
                         \"url\":\"https://console.cloud.google.com/storage/browser/inspr-cli/bin/insprcli-darwin-amd64-$VERSION\"}"
