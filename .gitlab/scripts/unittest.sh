ec303ce33a94b2f4983a150efde385e77f1b148a
Francisco Andrade <francisco@inspr.com>
2021-03-08 14:21:59 -0300

    Dev: removed mocks/fakes/examples/demos/main.go from test coverage

---
+++ .gitlab/scripts/unittest.sh
@@ -0,0 +1,3 @@
+go test -v -coverprofile=profile.cov.tmp -coverpkg=./... ./...
+cat profile.cov.tmp | grep -v 'fake\|mock\|examples\|main.go\|testutils' > profile.cov
+go tool cover -func profile.cov
