56de30f12c534c5f5371c198bd6c8d30973f83e3
Francisco Andrade <francisco@inspr.com>
2021-03-08 14:33:53 -0300

    Fix: added bash shebang

--- .gitlab/scripts/unittest.sh
+++ .gitlab/scripts/unittest.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 go test -v -coverprofile=profile.cov.tmp -coverpkg=./... ./...
 cat profile.cov.tmp | grep -v 'fake\|mock\|examples\|main.go\|testutils' > profile.cov
 go tool cover -func profile.cov
