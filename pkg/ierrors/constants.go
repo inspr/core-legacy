3aeb0e24fb06e9a3b59f8b0aafc11100c2a28ef4
Pedro <ptcar@inspr.com>
2021-03-09 10:05:52 -0300

    feat: multi error

--- pkg/ierrors/constants.go
+++ pkg/ierrors/constants.go
@@ -5,7 +5,7 @@ type InsprErrorCode int32
 
 // Error codes for inspr errors
 const (
-	NotFound InsprErrorCode = iota + 1
+	NotFound InsprErrorCode = iota << 1
 	AlreadyExists
 	InternalServer
 	InvalidName
