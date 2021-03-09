fea38f1537c9191cf421f03fc8d77b7f45565ed2
Francisco Andrade <francisco@inspr.com>
2021-03-09 11:27:10 -0300

    Print gcloud auth cfg

--- .gitlab/scripts/pushcli.sh
+++ .gitlab/scripts/pushcli.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 VERSION=$1
 
+echo $GCR_ACCOUNT_KEY
+
 gcloud auth configure-docker
 echo $GCR_ACCOUNT_KEY > $CI_PIPELINE_ID.json
 gcloud auth activate-service-account --key-file $CI_PIPELINE_ID.json
