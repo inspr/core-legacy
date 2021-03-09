163d9ce410f6de8d43369208047e336aa975f694
Francisco Andrade <francisco@inspr.com>
2021-03-09 10:31:12 -0300

    Fix: fixed version in build stage

--- .gitlab/scripts/pushcli.sh
+++ .gitlab/scripts/pushcli.sh
@@ -3,6 +3,7 @@ VERSION=$1
 
 gcloud auth configure-docker
 echo $GCR_ACCOUNT_KEY > /tmp/$CI_PIPELINE_ID.json
+cat /tmp/$CI_PIPELINE_ID.json
 gcloud auth activate-service-account --key-file /tmp/$CI_PIPELINE_ID.json
 export GOOGLE_APPLICATION_CREDENTIALS=/tmp/$CI_PIPELINE_ID.json
 gcloud config set project red-inspr
