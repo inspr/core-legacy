c08d952a3cb3a3cb18f2bb6b0e8dff8d28a8e83d
luiz-couto <luiz.felipe.556@outlook.com>
2021-03-09 13:24:41 -0300

    delete cmd test

--- cmd/inspr/cli/delete_test.go
+++ cmd/inspr/cli/delete_test.go
@@ -13,17 +13,129 @@ import (
 	"github.com/spf13/cobra"
 	cliutils "gitlab.inspr.dev/inspr/core/cmd/inspr/cli/utils"
 	"gitlab.inspr.dev/inspr/core/cmd/insprd/api/models"
-	"gitlab.inspr.dev/inspr/core/pkg/cmd"
+	"gitlab.inspr.dev/inspr/core/pkg/ierrors"
 	"gitlab.inspr.dev/inspr/core/pkg/meta"
-	"gitlab.inspr.dev/inspr/core/pkg/meta/utils"
+	"gitlab.inspr.dev/inspr/core/pkg/meta/utils/diff"
 	"gitlab.inspr.dev/inspr/core/pkg/rest"
 )
 
-func restartScopeFlag() {
-	cmd.InsprOptions.Scope = ""
+func getMockAppWithoutApp1() *meta.App {
+	root := meta.App{
+		Meta: meta.Metadata{
+			Name:        "appParent",
+			Reference:   "appParent",
+			Annotations: map[string]string{},
+			Parent:      "",
+			SHA256:      "",
+		},
+		Spec: meta.AppSpec{
+			Node: meta.Node{},
+			Apps: map[string]*meta.App{},
+			Channels: map[string]*meta.Channel{
+				"ch1": {
+					Meta: meta.Metadata{
+						Name:   "ch1",
+						Parent: "",
+					},
+					Spec: meta.ChannelSpec{
+						Type: "ct1",
+					},
+				},
+			},
+			ChannelTypes: map[string]*meta.ChannelType{
+				"ct1": {
+					Meta: meta.Metadata{
+						Name:        "ct1",
+						Reference:   "root.ct1",
+						Annotations: map[string]string{},
+						Parent:      "root",
+						SHA256:      "",
+					},
+					Schema: "",
+				},
+			},
+			Boundary: meta.AppBoundary{
+				Input:  []string{},
+				Output: []string{},
+			},
+		},
+	}
+	return &root
 }
 
-func getMockApp() *meta.App {
+func getMockAppWithoutCh1() *meta.App {
+	root := meta.App{
+		Meta: meta.Metadata{
+			Name:        "appParent",
+			Reference:   "appParent",
+			Annotations: map[string]string{},
+			Parent:      "",
+			SHA256:      "",
+		},
+		Spec: meta.AppSpec{
+			Node: meta.Node{},
+			Apps: map[string]*meta.App{
+				"app1": {
+					Meta: meta.Metadata{
+						Name:        "app1",
+						Reference:   "app1",
+						Annotations: map[string]string{},
+						Parent:      "",
+						SHA256:      "",
+					},
+					Spec: meta.AppSpec{
+						Node: meta.Node{},
+						Apps: map[string]*meta.App{
+							"thenewapp": {
+								Meta: meta.Metadata{
+									Name:        "thenewapp",
+									Reference:   "app1.thenewapp",
+									Annotations: map[string]string{},
+									Parent:      "app1",
+									SHA256:      "",
+								},
+								Spec: meta.AppSpec{
+									Apps:         map[string]*meta.App{},
+									Channels:     map[string]*meta.Channel{},
+									ChannelTypes: map[string]*meta.ChannelType{},
+									Boundary: meta.AppBoundary{
+										Input:  []string{"ch1app1"},
+										Output: []string{},
+									},
+								},
+							},
+						},
+						Channels:     map[string]*meta.Channel{},
+						ChannelTypes: map[string]*meta.ChannelType{},
+						Boundary: meta.AppBoundary{
+							Input:  []string{"ch1"},
+							Output: []string{"ch1"},
+						},
+					},
+				},
+			},
+			Channels: map[string]*meta.Channel{
+				"ch1": {
+					Meta: meta.Metadata{
+						Name:   "ch1",
+						Parent: "",
+					},
+					Spec: meta.ChannelSpec{
+						Type: "ct1",
+					},
+				},
+			},
+			ChannelTypes: map[string]*meta.ChannelType{},
+			Boundary: meta.AppBoundary{
+				Input:  []string{},
+				Output: []string{},
+			},
+		},
+	}
+	return &root
+}
+
+func getMockAppWithoutCt1() *meta.App {
 	root := meta.App{
 		Meta: meta.Metadata{
 			Name:        "appParent",
@@ -94,18 +206,7 @@ func getMockApp() *meta.App {
 					},
 				},
 			},
-			ChannelTypes: map[string]*meta.ChannelType{
-				"ct1": {
-					Meta: meta.Metadata{
-						Name:        "ct1",
-						Reference:   "root.ct1",
-						Annotations: map[string]string{},
-						Parent:      "root",
-						SHA256:      "",
-					},
-					Schema: "",
-				},
-			},
+			ChannelTypes: map[string]*meta.ChannelType{},
 			Boundary: meta.AppBoundary{
 				Input:  []string{},
 				Output: []string{},
@@ -115,24 +216,24 @@ func getMockApp() *meta.App {
 	return &root
 }
 
-func TestNewDescribeCmd(t *testing.T) {
+func TestNewDeleteCmd(t *testing.T) {
 	defer restartScopeFlag()
 	tests := []struct {
 		name          string
 		checkFunction func(t *testing.T, got *cobra.Command)
 	}{
 		{
-			name: "It should create a new describe command",
+			name: "It should create a new delete command",
 			checkFunction: func(t *testing.T, got *cobra.Command) {
 				if got == nil {
-					t.Errorf("NewDescribeCmd() not created successfully")
+					t.Errorf("NewDeleteCmd() not created successfully")
 				}
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := NewDescribeCmd()
+			got := NewDeleteCmd()
 			if tt.checkFunction != nil {
 				tt.checkFunction(t, got)
 			}
@@ -140,101 +241,12 @@ func TestNewDescribeCmd(t *testing.T) {
 	}
 }
 
-func Test_getScope(t *testing.T) {
-	tests := []struct {
-		name    string
-		want    string
-		wantErr bool
-	}{
-		{
-			name:    "It should return the scope",
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := cliutils.GetScope()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("getScope() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("getScope() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_processArg(t *testing.T) {
-	type args struct {
-		arg   string
-		scope string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    string
-		want1   string
-		wantErr bool
-	}{
-		{
-			name: "Arg is a invalid structure name - it should return a error",
-			args: args{
-				arg:   "invalid!name",
-				scope: "",
-			},
-			wantErr: true,
-		},
-		{
-			name: "Arg is a valid structure name",
-			args: args{
-				arg:   "helloWorld",
-				scope: "app1",
-			},
-			want:    "app1",
-			want1:   "helloWorld",
-			wantErr: false,
-		},
-		{
-			name: "Arg is a invalid scope - it should return a error",
-			args: args{
-				arg:   "hello..World",
-				scope: "app1",
-			},
-			wantErr: true,
-		},
-		{
-			name: "Arg is a valid scope",
-			args: args{
-				arg:   "hello.World",
-				scope: "app1",
-			},
-			want:    "app1.hello",
-			want1:   "World",
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, got1, err := cliutils.ProcessArg(tt.args.arg, tt.args.scope)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("processArg() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("processArg() got = %v, want %v", got, tt.want)
-			}
-			if got1 != tt.want1 {
-				t.Errorf("processArg() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
-}
-
-func Test_displayAppState(t *testing.T) {
+func Test_deleteApps(t *testing.T) {
 	defer restartScopeFlag()
 	bufResp := bytes.NewBufferString("")
-	utils.PrintAppTree(getMockApp(), bufResp)
+	changelog, _ := diff.Diff(getMockApp(), getMockAppWithoutApp1())
+
+	changelog.Print(bufResp)
 	outResp, _ := ioutil.ReadAll(bufResp)
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
@@ -246,9 +258,12 @@ func Test_displayAppState(t *testing.T) {
 			fmt.Println(err)
 		}
 
-		app := getMockApp()
+		if data.Ctx != "appParent.app1" {
+			rest.ERROR(w, ierrors.NewError().Message("error test").Build())
+			return
+		}
 
-		rest.JSON(w, http.StatusOK, app)
+		rest.JSON(w, http.StatusOK, changelog)
 	}
 
 	tests := []struct {
@@ -258,8 +273,8 @@ func Test_displayAppState(t *testing.T) {
 		expectedOutput []byte
 	}{
 		{
-			name:           "Should describe the app state",
-			flagsAndArgs:   []string{"a", "appParent"},
+			name:           "Should delete the app and return the diff",
+			flagsAndArgs:   []string{"a", "appParent.app1"},
 			handlerFunc:    handler,
 			expectedOutput: outResp,
 		},
@@ -271,7 +286,7 @@ func Test_displayAppState(t *testing.T) {
 		},
 		{
 			name:           "Valid scope flag",
-			flagsAndArgs:   []string{"a", "", "--scope", "appParent"},
+			flagsAndArgs:   []string{"a", "", "--scope", "appParent.app1"},
 			handlerFunc:    handler,
 			expectedOutput: outResp,
 		},
@@ -284,7 +299,7 @@ func Test_displayAppState(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			cmd := NewDescribeCmd()
+			cmd := NewDeleteCmd()
 			buf := bytes.NewBufferString("")
 
 			cliutils.SetOutput(buf)
@@ -299,16 +314,18 @@ func Test_displayAppState(t *testing.T) {
 			got, _ := ioutil.ReadAll(buf)
 
 			if !reflect.DeepEqual(got, tt.expectedOutput) {
-				t.Errorf("displayAppState() = %v, want %v", string(got), string(tt.expectedOutput))
+				t.Errorf("deleteApps() = %v, want %v", string(got), string(tt.expectedOutput))
 			}
 		})
 	}
 }
 
-func Test_displayChannelState(t *testing.T) {
+func Test_deleteChannels(t *testing.T) {
 	defer restartScopeFlag()
 	bufResp := bytes.NewBufferString("")
-	utils.PrintChannelTree(getMockApp().Spec.Channels["ch1"], bufResp)
+	changelog, _ := diff.Diff(getMockApp(), getMockAppWithoutCh1())
+
+	changelog.Print(bufResp)
 	outResp, _ := ioutil.ReadAll(bufResp)
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
@@ -320,9 +337,12 @@ func Test_displayChannelState(t *testing.T) {
 			fmt.Println(err)
 		}
 
-		ch := getMockApp().Spec.Channels[data.ChName]
+		if data.Ctx != "appParent" || data.ChName != "ch1" {
+			rest.ERROR(w, ierrors.NewError().Message("error test").Build())
+			return
+		}
 
-		rest.JSON(w, http.StatusOK, ch)
+		rest.JSON(w, http.StatusOK, changelog)
 	}
 
 	tests := []struct {
@@ -332,14 +352,14 @@ func Test_displayChannelState(t *testing.T) {
 		expectedOutput []byte
 	}{
 		{
-			name:           "Should describe the channel state",
+			name:           "Should delete the channel and return the diff",
 			flagsAndArgs:   []string{"ch", "appParent.ch1"},
 			handlerFunc:    handler,
 			expectedOutput: outResp,
 		},
 		{
 			name:           "Invalid scope flag, should not print",
-			flagsAndArgs:   []string{"ch", "ch1", "--scope", "invalid..scope"},
+			flagsAndArgs:   []string{"ch", "appParent.ch1", "--scope", "invalid..scope"},
 			handlerFunc:    handler,
 			expectedOutput: []byte(""),
 		},
@@ -351,14 +371,14 @@ func Test_displayChannelState(t *testing.T) {
 		},
 		{
 			name:           "Invalid arg",
-			flagsAndArgs:   []string{"ch", "invalid..args", "--scope", "appParent"},
+			flagsAndArgs:   []string{"ch", "invalid..args", "--scope", "appParent.ch1"},
 			handlerFunc:    handler,
 			expectedOutput: []byte(""),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			cmd := NewDescribeCmd()
+			cmd := NewDeleteCmd()
 			buf := bytes.NewBufferString("")
 
 			cliutils.SetOutput(buf)
@@ -373,16 +393,18 @@ func Test_displayChannelState(t *testing.T) {
 			got, _ := ioutil.ReadAll(buf)
 
 			if !reflect.DeepEqual(got, tt.expectedOutput) {
-				t.Errorf("displayChannelState() = %v, want %v", string(got), string(tt.expectedOutput))
+				t.Errorf("deleteChannels() = %v, want %v", string(got), string(tt.expectedOutput))
 			}
 		})
 	}
 }
 
-func Test_displayChannelTypeState(t *testing.T) {
+func Test_deleteCTypes(t *testing.T) {
 	defer restartScopeFlag()
 	bufResp := bytes.NewBufferString("")
-	utils.PrintChannelTypeTree(getMockApp().Spec.ChannelTypes["ct1"], bufResp)
+	changelog, _ := diff.Diff(getMockApp(), getMockAppWithoutCt1())
+
+	changelog.Print(bufResp)
 	outResp, _ := ioutil.ReadAll(bufResp)
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
@@ -394,9 +416,12 @@ func Test_displayChannelTypeState(t *testing.T) {
 			fmt.Println(err)
 		}
 
-		ct := getMockApp().Spec.ChannelTypes[data.CtName]
+		if data.Ctx != "appParent" || data.CtName != "ct1" {
+			rest.ERROR(w, ierrors.NewError().Message("error test").Build())
+			return
+		}
 
-		rest.JSON(w, http.StatusOK, ct)
+		rest.JSON(w, http.StatusOK, changelog)
 	}
 
 	tests := []struct {
@@ -406,14 +431,14 @@ func Test_displayChannelTypeState(t *testing.T) {
 		expectedOutput []byte
 	}{
 		{
-			name:           "Should describe the channelType state",
+			name:           "Should delete the channelType and return the diff",
 			flagsAndArgs:   []string{"ct", "appParent.ct1"},
 			handlerFunc:    handler,
 			expectedOutput: outResp,
 		},
 		{
 			name:           "Invalid scope flag, should not print",
-			flagsAndArgs:   []string{"ct", "ct1", "--scope", "invalid..scope"},
+			flagsAndArgs:   []string{"ct", "appParent.ct1", "--scope", "invalid..scope"},
 			handlerFunc:    handler,
 			expectedOutput: []byte(""),
 		},
@@ -425,14 +450,14 @@ func Test_displayChannelTypeState(t *testing.T) {
 		},
 		{
 			name:           "Invalid arg",
-			flagsAndArgs:   []string{"ct", "invalid..args", "--scope", "appParent"},
+			flagsAndArgs:   []string{"ct", "invalid..args", "--scope", "appParent.ct1"},
 			handlerFunc:    handler,
 			expectedOutput: []byte(""),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			cmd := NewDescribeCmd()
+			cmd := NewDeleteCmd()
 			buf := bytes.NewBufferString("")
 
 			cliutils.SetOutput(buf)
@@ -447,7 +472,7 @@ func Test_displayChannelTypeState(t *testing.T) {
 			got, _ := ioutil.ReadAll(buf)
 
 			if !reflect.DeepEqual(got, tt.expectedOutput) {
-				t.Errorf("displayChannelTypeState() = %v, want %v", string(got), string(tt.expectedOutput))
+				t.Errorf("deleteCtypes() = %v, want %v", string(got), string(tt.expectedOutput))
 			}
 		})
 	}
