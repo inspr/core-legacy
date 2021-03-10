082dd32e63b10a80fbe4cde05c4e4c5905a3938f
luiz-couto <luiz.felipe.556@outlook.com>
2021-03-10 14:39:14 -0300

    removed debug print

--- cmd/inspr/cli/apply.go
+++ cmd/inspr/cli/apply.go
@@ -89,8 +89,6 @@ func doApply(_ context.Context) error {
 		return ierrors.NewError().Message("invalid flag arguments").Build()
 	}
 
-	fmt.Println(cmd.InsprOptions.AppliedFileStructure)
-
 	if hasFileFlag {
 		files = append(files, cmd.InsprOptions.AppliedFileStructure)
 	} else {
