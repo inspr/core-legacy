74c1c68943b6d1b33f8fccfc3925f08995ac7bf3
Pedro <ptcar@inspr.com>
2021-03-12 13:03:05 -0300

    fix: fixes reaction for aliases

--- cmd/insprd/api/handlers/reactions.go
+++ cmd/insprd/api/handlers/reactions.go
@@ -179,8 +179,8 @@ var updatedAliases func(handler *Handler) diff.DifferenceReaction = func(handler
 			appName, boundaryName, _ := utils.RemoveLastPartInScope(d.Name)
 			newScope, _ := utils.JoinScopes(scope, appName)
 			app, err := handler.Memory.Apps().Get(newScope)
-			if err != nil && app.Spec.Boundary.Input.Union(app.Spec.Boundary.Output).Contains(boundaryName) {
-				_, err := handler.Operator.Nodes().CreateNode(context.Background(), app)
+			if err == nil && app.Spec.Boundary.Input.Union(app.Spec.Boundary.Output).Contains(boundaryName) {
+				_, err := handler.Operator.Nodes().UpdateNode(context.Background(), app)
 				if err != nil {
 					return err
 				}
