d91b7ba5ea813a8d8a9c38dbc63177e855985716
luiz-couto <luiz.felipe.556@outlook.com>
2021-03-12 14:00:39 -0300

    renamed fake

--- cmd/insprd/memory/fake/alias.go
+++ cmd/insprd/memory/fake/alias.go
@@ -10,21 +10,21 @@ type Alias struct {
 }
 
 // GetAlias - simple mock
-func (ch *Alias) GetAlias(context string, aliasKey string) (*meta.Alias, error) {
+func (a *Alias) GetAlias(context string, aliasKey string) (*meta.Alias, error) {
 	return &meta.Alias{}, nil
 }
 
 // CreateAlias - simple mock
-func (ch *Alias) CreateAlias(query string, targetBoundary string, alias *meta.Alias) error {
+func (a *Alias) CreateAlias(query string, targetBoundary string, alias *meta.Alias) error {
 	return nil
 }
 
 // DeleteAlias - simple mock
-func (ch *Alias) DeleteAlias(context string, aliasKey string) error {
+func (a *Alias) DeleteAlias(context string, aliasKey string) error {
 	return nil
 }
 
 // UpdateAlias - simple mock
-func (ch *Alias) UpdateAlias(contexcontext string, aliasKey string, alias *meta.Alias) error {
+func (a *Alias) UpdateAlias(contexcontext string, aliasKey string, alias *meta.Alias) error {
 	return nil
 }
