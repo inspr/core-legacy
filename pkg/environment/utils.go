fbf9e0f595daab2c8aa00de7c125299150948d62
Pedro <ptcar@inspr.com>
2021-03-16 11:49:53 -0300

    ref: changes types to use string array
    changes types to use string array
    
    changes schema getting to lookup the environment variable

--- pkg/environment/utils.go
+++ pkg/environment/utils.go
@@ -21,16 +21,16 @@ func IsInOutputChannel(channel, outputChan string) bool {
 }
 
 // GetInputChannelList returns a string list with the channels in insprEnv.InputChannels
-func GetInputChannelList(inputChan string) []string {
+func GetInputChannelList(inputChan string) utils.StringArray {
 	if inputChan == "" {
-		return []string{}
+		return utils.StringArray{}
 	}
 	arr := strings.Split(inputChan, ";")
 	return arr
 }
 
 // GetResolvedInputChannelList gets the list of resolved channels from the input boundary
-func GetResolvedInputChannelList(inputChan string) []string {
+func GetResolvedInputChannelList(inputChan string) utils.StringArray {
 	arr := utils.StringArray(GetInputChannelList(inputChan))
 	return arr.Map(func(s string) string {
 		resolved, _ := GetResolvedChannel(s, inputChan, "")
@@ -39,7 +39,7 @@ func GetResolvedInputChannelList(inputChan string) []string {
 }
 
 // GetResolvedOutputChannelList gets the list of resolved channels from the output boundary
-func GetResolvedOutputChannelList(outputChan string) []string {
+func GetResolvedOutputChannelList(outputChan string) utils.StringArray {
 	arr := utils.StringArray(GetOutputChannelList(outputChan))
 	return arr.Map(func(s string) string {
 		resolved, _ := GetResolvedChannel(s, outputChan, "")
@@ -48,23 +48,21 @@ func GetResolvedOutputChannelList(outputChan string) []string {
 }
 
 // GetOutputChannelList returns a string list with the channels in insprEnv.OutputChannels
-func GetOutputChannelList(outputChan string) []string {
+func GetOutputChannelList(outputChan string) utils.StringArray {
 	if outputChan == "" {
-		return []string{}
+		return utils.StringArray{}
 	}
 	arr := strings.Split(outputChan, ";")
 	return arr
 }
 
 // GetSchema returns a channel's schema, if the channel exists
-func GetSchema(channel, inputChan, outputChan string) (string, error) {
-	if IsInInputChannel(channel, inputChan) || IsInOutputChannel(channel, outputChan) {
-		return os.Getenv(channel + "_SCHEMA"), nil
+func GetSchema(channel string) (string, error) {
+	schema, ok := os.LookupEnv(channel + "_SCHEMA")
+	if !ok {
+		return "", ierrors.NewError().NotFound().Message("schema for channel %s not found", channel).Build()
 	}
-	return "", ierrors.NewError().
-		InvalidChannel().
-		Message("channel " + channel + " not listed as an input or output").
-		Build()
+	return schema, nil
 }
 
 // GetResolvedChannel gets a resolved channel from a channel name
