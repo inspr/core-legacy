d41ae86ea75d92df457a7604a8027b616ab22a5a
Nicholas Cristofaro <nicholas@inspr.com>
2021-03-19 12:59:25 -0300

    quick fix of the install script, wasn't identifying the macos config

--- .gitlab/scripts/install_cli.sh
+++ .gitlab/scripts/install_cli.sh
@@ -3,28 +3,31 @@
 CLI_VERSION=$(curl -s https://storage.googleapis.com/inspr-cli/latest-version)
 CURL_URL="https://storage.googleapis.com/inspr-cli/${CLI_VERSION}/insprcli"
 
-OS_NAME=$(uname -o)
+OS_NAME=$(uname -s)
 echo 'Your operating system is '$OS_NAME
-if [[ $OS_NAME == 'GNU/Linux' ]]; then # windows
-    CURL_URL=$CURL_URL"-linux"
-elif [[ $OS_NAME == 'Windows' ]]; then # linux
-    CURL_URL=$CURL_URL"-windows"
-elif  [[ $OS_NAME == 'Darwin' ]]; then  # darwin
-    CURL_URL=$CURL_URL"-darwin"
-fi
+
+case "${OS_NAME}" in
+    Linux*)     CURL_URL=$CURL_URL"-linux";;
+    Darwin*)    CURL_URL=$CURL_URL"-darwin";;
+    CYGWIN*)    CURL_URL=$CURL_URL"-windows";;
+    MINGW*)     CURL_URL=$CURL_URL"-windows";;
+    Windows*)   CURL_URL=$CURL_URL"-windows";;
+    *)          echo "ERROR identifing the os"
+esac
+
 
 ARCH=$(uname -i)
 echo 'Your computer architecture is '$ARCH
-if [[ $ARCH == x86_64* ]]; then # x64
-    CURL_URL=$CURL_URL"-amd64"
-elif [[ $ARCH == i*86 ]]; then # x32
-    CURL_URL=$CURL_URL"-386"
-elif  [[ $ARCH == arm* ]] || [[ $ARCH = aarch64 ]]; then  # arm
-    CURL_URL=$CURL_URL"-arm64"
-fi
+
+case "${ARCH}" in
+    x86_64*) CURL_URL=$CURL_URL"-amd64";;
+    i*86) CURL_URL=$CURL_URL"-386";;
+    arm*) CURL_URL=$CURL_URL"-arm64";;
+    aarch64) CURL_URL=$CURL_URL"-arm64";;
+    *)  echo "ERROR identifing the architecture"
+esac
 
 CURL_URL=$CURL_URL"-"$CLI_VERSION
-
 echo 'Downloading the inspr cli binary'
 curl $CURL_URL -o /tmp/inspr
 
