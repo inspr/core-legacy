3715722c25a8a9119428cdeeda294b6cfa61acf4
Pedro <ptcar@inspr.com>
2021-03-26 13:03:19 -0300

    fix: add app versioning and removes unused tags

--- .gitlab/scripts/update_tags.sh
+++ .gitlab/scripts/update_tags.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 CHART_URL=$1
 CHART_GS_URI=$2
-
 echo "Updating helm chart tags"
 INSPRD_TAG=$(printf '%s' $(grep -Eo "gcr\.io\/red\-inspr\/insprd\:[^@]+\@sha256\:[[:alnum:]]+" tags.out) | sed -e 's/[\/&]/\\&/g')
 KAFKA_SIDECAR_TAG=$(printf '%s' $(grep -Eo "gcr\.io\/red\-inspr\/inspr\/sidecar\/kafka\:[^@]+\@sha256\:[[:alnum:]]+" tags.out) | sed -e 's/[\/&]/\\&/g')
@@ -14,11 +13,16 @@ echo "$KAFKA_SIDECAR_TAG"
 echo "Updating tags"
 sed -i 's/gcr\.io\/red\-inspr\/insprd/'"$INSPRD_TAG"'/'  build/helm/values.yaml
 sed -i 's/gcr\.io\/red\-inspr\/inspr\/sidecar\/kafka/'"$KAFKA_SIDECAR_TAG"'/' build/helm/values.yaml
+
+APP_VERSION=$(git describe --always --tags)
+echo "Updating chart app version to $APP_VERSION"
+sed -i 's/appVersion: .*/appVersion: '"$APP_VERSION"'/' build/helm/Chart.yaml
+
+
 VERSION=$(grep -Po "version: \K.*" build/helm/Chart.yaml)
 NEW_VERSION=$(VERSION=$VERSION KIND=patch python .gitlab/scripts/upgrade_version.py)
 
 echo "Updating chart version to $NEW_VERSION"
-
 sed -i 's/version: .*/version: '"$NEW_VERSION"'/' build/helm/Chart.yaml
 
 
