07b15281ad093e3cca3a1cc6aa783883f8ec4bdf
Lucas <lucas@inspr.com>
2021-04-05 11:22:37 -0300

    Dev : new auth interface

--- cmd/insprd/auth/interface.go
+++ cmd/insprd/auth/interface.go
@@ -3,6 +3,4 @@ package auth
 //Auth is the inteface for interacting with the Authentication service
 type Auth interface {
 	Validade(token string) (bool, error)
-	Login(usr, pwd string) (User, error)
-	Register(usr User)
 }
