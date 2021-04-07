267e64d338bfcd63bdfe995fcf8809244fb8e763
Pedro <ptcar@inspr.com>
2021-04-07 14:22:13 -0300

    feat: CRUD handler helper

--- pkg/rest/middleware.go
+++ pkg/rest/middleware.go
@@ -9,3 +9,33 @@ func (h Handler) JSON() Handler {
 		h(w, r)
 	}
 }
+
+type CRUDHandler interface {
+	HandleCreate() Handler
+	HandleDelete() Handler
+	HandleUpdate() Handler
+	HandleGet() Handler
+}
+
+func HandleCRUD(handler CRUDHandler) Handler {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+
+		case http.MethodGet:
+			handler.HandleGet().JSON().Recover()(w, r)
+
+		case http.MethodPost:
+			handler.HandleCreate().JSON().Recover()(w, r)
+
+		case http.MethodPut:
+			handler.HandleUpdate().JSON().Recover()(w, r)
+
+		case http.MethodDelete:
+			handler.HandleDelete().JSON().Recover()(w, r)
+
+		default:
+			http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}
+}
