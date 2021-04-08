c777de503f15ace205830a18c95d0d356d37415d
Nicholas Cristofaro <nicholas@inspr.com>
2021-04-08 15:18:01 -0300

    sketching before merging with lucas' issue

--- pkg/rest/middleware.go
+++ pkg/rest/middleware.go
@@ -1,7 +1,10 @@
 package rest
 
 import (
+	b64 "encoding/base64"
+	"encoding/json"
 	"net/http"
+	"strings"
 
 	authentication "gitlab.inspr.dev/inspr/core/cmd/insprd/auth"
 	"gitlab.inspr.dev/inspr/core/pkg/ierrors"
@@ -22,14 +25,15 @@ func (h Handler) Validate(auth authentication.Auth) Handler {
 		if len(headerContent) != 2 {
 			http.Error(
 				w,
-				"Missing token",
+				"Bad Request, expected: Authorization: Bearer <token>",
 				http.StatusBadRequest,
 			)
 			return
 		}
 
 		token := headerContent[1]
-		_, err := auth.Validade(token)
+		// TODO REWRITE THIS WITH THE []BYTE RETURN
+		payload, err := auth.Validade(token)
 
 		// error management
 		if err != nil {
@@ -53,16 +57,6 @@ func (h Handler) Validate(auth authentication.Auth) Handler {
 				return
 			}
 
-			// check for unauthorized error
-			if ierrors.HasCode(err, ierrors.Unauthorized) {
-				http.Error(
-					w,
-					"Unauthorized to do operations in this context",
-					http.StatusForbidden,
-				)
-				return
-			}
-
 			// default error message
 			http.Error(
 				w,
@@ -71,6 +65,34 @@ func (h Handler) Validate(auth authentication.Auth) Handler {
 			)
 			return
 		}
+
+		// payload data
+		var pl authentication.Payload
+		payloadBytes, _ := b64.StdEncoding.DecodeString(payload)
+		json.Unmarshal(payloadBytes, &pl)
+
+		// TODO rewrite this to get from the HEADER
+		// request scope
+		data := struct {
+			Scope string `json:"scope"`
+		}{}
+		// TODO this will be reading the body, work on a copy
+		json.NewDecoder(r.Body).Decode(&data)
+
+		for _, scope := range pl.Scope {
+			if strings.Contains(scope, data.Scope) {
+				// scope found
+			}
+		}
+		// check for unauthorized error
+		// if ierrors.HasCode(err, ierrors.Unauthorized) {
+		// 	http.Error(
+		// 		w,
+		// 		"Unauthorized to do operations in this context",
+		// 		http.StatusForbidden,
+		// 	)
+		// 	return
+		// }
 
 		// token and context are valid
 		h(w, r)
