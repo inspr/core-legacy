eff6a47b6e8374c053b89b31e54d8271ade019cf
Pedro <ptcar@inspr.com>
2021-04-08 15:15:33 -0300

    ref: modularizes interface

--- cmd/uid_provider/client/interface.go
+++ cmd/uid_provider/client/interface.go
@@ -18,13 +18,16 @@ type User struct {
 }
 
 type RedisManager interface {
+	UIDClient
+	RefreshToken(ctx context.Context, refreshToken string) (Payload, error)
+}
+
+type UIDClient interface {
 	// creates payload and sends it to insprd
 	// when creating the payload, generetes the Refresh Token (cryptografado)
-	Login(ctx context.Context, uid, pwd string) error // asks Insprd to generate token and saves it into file
+	Login(ctx context.Context, uid, pwd string) (string, error) // asks Insprd to generate token and saves it into file
 
 	CreateUser(ctx context.Context, uid string, newUser User) error
 	DeleteUser(ctx context.Context, uid, usrToBeDeleted string) error
 	UpdatePassword(ctx context.Context, uid, usrToBeUpdated, newPwd string) error
-
-	RefreshToken(ctx context.Context, refreshToken string) (Payload, error)
 }
