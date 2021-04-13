04dc6f21b9f839c41e31eb2e7c3ddf74062dafaf
Francisco Andrade <francisco@inspr.com>
2021-04-13 08:49:36 -0300

    Test: tested auth controller

--- pkg/controller/client/auth.go
+++ pkg/controller/client/auth.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"gitlab.inspr.dev/inspr/core/pkg/api/auth"
+	"gitlab.inspr.dev/inspr/core/pkg/api/models"
 	"gitlab.inspr.dev/inspr/core/pkg/rest/request"
 )
 
@@ -15,15 +16,12 @@ type AuthClient struct {
 // GenerateToken sends a request containing a payload so Insprd
 // generates a new auth token based on the payload's info
 func (ac *AuthClient) GenerateToken(ctx context.Context, payload auth.Payload) (string, error) {
-	type returnStructure struct {
-		Token string `json:"token"`
-	}
-	var resp returnStructure
+	authDI := models.AuthDI{}
 
-	err := ac.c.Send(ctx, "/auth", "POST", payload, &resp)
+	err := ac.c.Send(ctx, "/auth", "POST", payload, &authDI)
 	if err != nil {
 		return "", err
 	}
 
-	return resp.Token, nil
+	return authDI.Token, nil
 }
