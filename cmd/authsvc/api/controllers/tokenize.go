3a63f8eefb3b9730ea7966d68e5fd54af4d3574b
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2021-04-14 10:30:36 -0300

    Fix: log error

--- cmd/authsvc/api/controllers/tokenize.go
+++ cmd/authsvc/api/controllers/tokenize.go
@@ -52,7 +52,7 @@ func (server *Server) tokenize(payload models.Payload) ([]byte, error) {
 
 	var privPemBytes []byte
 	if privKey.Type != "RSA PRIVATE KEY" {
-		server.logger.Info("RSA private key is of the wrong type")
+		server.logger.Error("RSA private key is of the wrong type")
 		err := ierrors.NewError().InternalServer().Message("RSA private key is of the wrong type").Build()
 		return nil, err
 	}
@@ -62,7 +62,7 @@ func (server *Server) tokenize(payload models.Payload) ([]byte, error) {
 	var parsedKey interface{}
 	if parsedKey, err = x509.ParsePKCS1PrivateKey(privPemBytes); err != nil {
 		if parsedKey, err = x509.ParsePKCS8PrivateKey(privPemBytes); err != nil { // note this returns type `interface{}`
-			server.logger.Info("Unable to parse RSA private key")
+			server.logger.Error("Unable to parse RSA private key")
 			err := ierrors.NewError().InternalServer().Message("Unable to parse RSA private key").Build()
 			return nil, err
 		}
@@ -70,21 +70,21 @@ func (server *Server) tokenize(payload models.Payload) ([]byte, error) {
 
 	privateKey, ok := parsedKey.(*rsa.PrivateKey)
 	if !ok {
-		server.logger.Info("Unable to parse RSA private key")
+		server.logger.Error("Unable to parse RSA private key")
 		err := ierrors.NewError().InternalServer().Message("Unable to parse RSA private key").Build()
 		return nil, err
 	}
 
 	err = privateKey.Validate()
 	if err != nil {
-		server.logger.Info("Unable to validate private key", zap.Any("error", err))
+		server.logger.Error("Unable to validate private key", zap.Any("error", err))
 		err := ierrors.NewError().InternalServer().Message("Unable to validate private key").Build()
 		return nil, err
 	}
 
 	signed, err := jwt.Sign(token, jwa.RS256, privateKey)
 	if err != nil {
-		server.logger.Info("Unable to sign JWT with provided RSA private key", zap.Any("error", err))
+		server.logger.Error("Unable to sign JWT with provided RSA private key", zap.Any("error", err))
 		err := ierrors.NewError().InternalServer().Message("Unable to sign JWT with provided RSA private key").Build()
 		return nil, err
 	}
