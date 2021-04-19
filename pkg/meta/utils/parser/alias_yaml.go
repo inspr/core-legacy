3907ee4634a45ec975da2554b2886aa527a60a1e
luiz-couto <luiz.felipe.556@outlook.com>
2021-04-19 10:37:18 -0300

    yaml to alias test

--- pkg/meta/utils/parser/alias_yaml.go
+++ pkg/meta/utils/parser/alias_yaml.go
@@ -7,16 +7,16 @@ import (
 )
 
 // YamlToAlias - deserializes the yaml to a meta.Alias struct
-func YamlToAlias(bytes []byte) (meta.Alias, error) {
+func YamlToAlias(bytes []byte) (*meta.Alias, error) {
 	var alias *meta.Alias
 
 	if err := yaml.Unmarshal(bytes, &alias); err != nil {
-		return *alias, ierrors.NewError().Message("Error parsing the file").Build()
+		return nil, ierrors.NewError().Message("Error parsing the file").Build()
 	}
 
 	if alias.Meta.Name == "" {
-		return *alias, ierrors.NewError().Message("alias without name").Build()
+		return nil, ierrors.NewError().Message("alias without name").Build()
 	}
 
-	return *alias, nil
+	return alias, nil
 }
