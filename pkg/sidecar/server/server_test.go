9e08d77a42d5d1d23f22a10692ca41717316d25f
Nicholas Cristofaro <nicholas@inspr.com>
2021-04-19 10:55:00 -0300

    changing the test of the TestServer_Run, unecessary go routine was removed

--- pkg/sidecar/server/server_test.go
+++ pkg/sidecar/server/server_test.go
@@ -78,42 +78,36 @@ func TestServer_Init(t *testing.T) {
 func TestServer_Run(t *testing.T) {
 	routes := []string{"commit", "writeMessage", "readMessage"}
 	env.SetMockEnv()
+	defer env.UnsetMockEnv()
+
+	// SERVER
+	s := MockServer(nil)
+	s.Init(s.Reader, s.Writer)
+	s.addr = "./test.sock"
+
+	// starts server
+	go func() {
+		s.Run(context.Background())
+	}()
+	// gives time for the server to start up
+	time.Sleep(500 * time.Millisecond)
+
+	// mock socket
+	c := transports.NewUnixSocketClient("./test.sock")
 
 	for _, r := range routes {
 		t.Run("run_test/"+r, func(t *testing.T) {
+			resp, err := c.Post("http://unix/"+r, "", nil)
+			if err != nil {
+				t.Errorf("Failed to make post to route '%v'", r)
+				return
+			}
+			if resp.StatusCode != http.StatusBadRequest {
+				t.Errorf("route '/commit' = %v, want %v", resp.StatusCode, http.StatusBadRequest)
+			}
 
-			// SERVER
-			var wg sync.WaitGroup
-			wg.Add(1)
-			defer wg.Wait()
-
-			s := MockServer(nil)
-			s.Init(s.Reader, s.Writer)
-			s.addr = "./test.sock"
-
-			go func() {
-				s.Run(context.Background())
-			}()
-
-			go func() {
-				defer wg.Done()
-				time.Sleep(500 * time.Microsecond)
-
-				// env mock socket addr
-				c := transports.NewUnixSocketClient("./test.sock")
-
-				resp, err := c.Post("http://unix/"+r, "", nil)
-				if err != nil {
-					t.Errorf("Failed to make post to route '/commit'")
-					return
-				}
-				if resp.StatusCode != http.StatusBadRequest {
-					t.Errorf("route '/commit' = %v, want %v", resp.StatusCode, http.StatusBadRequest)
-				}
-			}()
 		})
 	}
-	env.UnsetMockEnv()
 }
 
 func TestServer_Cancel(t *testing.T) {
