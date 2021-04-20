568747e6d5e55485ee86c3d8f1a87a57d1faa32a
Pedro <ptcar@pteco.dev>
2021-04-20 11:47:00 -0300

    lint: adds comment to get token function

--- pkg/cmd/utils/get_token.go
+++ pkg/cmd/utils/get_token.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 )
 
+// GetToken gets a token from a filepath, returns an empty token if file does not exist.
 func GetToken(path string) []byte {
 	content, err := ioutil.ReadFile(path)
 	if err != nil {
