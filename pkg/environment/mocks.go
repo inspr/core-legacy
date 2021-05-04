68bd96c2caee73248725fb60e72a30d383ed194c
Nicholas Cristofaro <nicholas@inspr.com>
2021-05-04 09:00:01 -0300

    fix(environment): adding kafka env variables, they are breaking the tests on github

--- pkg/environment/mocks.go
+++ pkg/environment/mocks.go
@@ -5,6 +5,7 @@ import "os"
 // SetMockEnv - sets the environment variables to specific values
 func SetMockEnv() {
 	channelsValues := "chan;chan1;chan2;chan3"
+	os.Setenv("INSPR_SIDECAR_PORT", "8888")
 	os.Setenv("INSPR_INPUT_CHANNELS", channelsValues)
 	os.Setenv("INSPR_OUTPUT_CHANNELS", channelsValues)
 	os.Setenv("INSPR_UNIX_SOCKET", "socket_addr")
@@ -14,15 +15,21 @@ func SetMockEnv() {
 
 	os.Setenv("INSPR_ENV", "mock_env")
 	os.Setenv("INSPR_APP_ID", "testappid1")
+	os.Setenv("INSPR_SIDECAR_WRITE_PORT", "3001")
+	os.Setenv("INSPR_SIDECAR_READ_PORT", "3002")
 }
 
 // UnsetMockEnv - removes the values of the environment variables
 func UnsetMockEnv() {
 	os.Unsetenv("INSPR_INPUT_CHANNELS")
+	os.Unsetenv("INSPR_SIDECAR_PORT")
 	os.Unsetenv("INSPR_OUTPUT_CHANNELS")
 	os.Unsetenv("INSPR_UNIX_SOCKET")
 	os.Unsetenv("INSPR_SIDECAR_IMAGE")
 	os.Unsetenv("INSPR_APP_CTX")
 	os.Unsetenv("INSPR_ENV")
 	os.Unsetenv("INSPR_APP_ID")
+	os.Unsetenv("INSPR_SIDECAR_WRITE_PORT")
+	os.Unsetenv("INSPR_SIDECAR_READ_PORT")
+	os.Unsetenv("INSPR_SIDECAR_PORT")
 }
