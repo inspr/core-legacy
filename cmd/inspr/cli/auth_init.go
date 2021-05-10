58287eb5ab6ccc16fa21a13c0f866d8f299ab1ce
Pedro Tavares de Carvalho <ptcar@pteco.dev>
2021-05-10 00:37:15 -0300

    fix(cli): changes description to match correct command on cluster init

--- cmd/inspr/cli/auth_init.go
+++ cmd/inspr/cli/auth_init.go
@@ -8,20 +8,22 @@ import (
 	"github.com/inspr/inspr/pkg/cmd/utils"
 )
 
-var authCommand = cmd.NewCmd("cluster").AddSubCommand(
-	cmd.NewCmd("init").
-		WithDescription("Cluster configuration").
-		ExactArgs(1,
-			func(c context.Context, args []string) error {
-				token, err := utils.GetCliClient().Authorization().Init(c, args[0])
-				if err != nil {
-					return err
-				}
-				output := utils.GetCliOutput()
+var authCommand = cmd.NewCmd("cluster").
+	WithDescription("Cluster configuration").
+	AddSubCommand(
+		cmd.NewCmd("init").
+			WithDescription("Initialize the cluster and get the admin token").
+			ExactArgs(1,
+				func(c context.Context, args []string) error {
+					token, err := utils.GetCliClient().Authorization().Init(c, args[0])
+					if err != nil {
+						return err
+					}
+					output := utils.GetCliOutput()
 
-				fmt.Fprintln(output, "This is a root token for authentication within your insprd. This will not be generated again. Save it wisely.")
-				fmt.Fprintf(output, "%s\n", token)
-				return nil
-			},
-		),
-).WithCommonFlags().Super()
+					fmt.Fprintln(output, "This is a root token for authentication within your insprd. This will not be generated again. Save it wisely.")
+					fmt.Fprintf(output, "%s\n", token)
+					return nil
+				},
+			),
+	).WithCommonFlags().Super()
