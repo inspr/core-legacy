c7249fca5cd48f6bebbea5282dd7a26ad13fbd9c
Pedro Tavares de Carvalho <ptcar@pteco.dev>
2021-05-10 17:49:11 -0300

    feat(hooks): adds pre-commit hook and hook wrappers

---
+++ scripts/install-hooks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#!/bin/bash
+HOOK_NAMES="applypatch-msg pre-applypatch post-applypatch pre-commit prepare-commit-msg commit-msg post-commit pre-rebase post-checkout post-merge pre-receive update post-receive post-update pre-auto-gc"
+# assuming the script is in a bin directory, one level into the repo
+HOOK_DIR=$(git rev-parse --show-toplevel)/.git/hooks
+SCRIPTS_DIR=$(git rev-parse --show-toplevel)/scripts
+
+for hook in $HOOK_NAMES; do
+    # If the hook already exists, is executable, and is not a symlink
+    if [ ! -h $HOOK_DIR/$hook -a -x $HOOK_DIR/$hook ]; then
+        mv $HOOK_DIR/$hook $HOOK_DIR/$hook.local
+    fi
+    # create the symlink, overwriting the file if it exists
+    # probably the only way this would happen is if you're using an old version of git
+    # -- back when the sample hooks were not executable, instead of being named ____.sample
+    ln -s -f $SCRIPTS_DIR/hooks-wrapper.sh $HOOK_DIR/$hook
+done
