40d2dd07b052aeead663d170f8e8da0ed853412f
Francisco Andrade <francisco@inspr.com>
2021-05-13 15:26:43 -0300

    fix(devops): fixed how secrets were passed to scripts

--- .github/scripts/pushcli.sh
+++ .github/scripts/pushcli.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+GCR_DETAILS=$1
+CI_RUN_ID=$2
 VERSION=$(cat version)
 
 gcloud auth configure-docker
-echo ${{ secret.GCR_ACCOUNT_KEY }} > ${{ github.run_id }}.json
-gcloud auth activate-service-account --key-file ${{ github.run_id }}.json
-export GOOGLE_APPLICATION_CREDENTIALS=${{ github.run_id }}.json
+echo GCR_DETAILS > CI_RUN_ID.json
+gcloud auth activate-service-account --key-file CI_RUN_ID.json
+export GOOGLE_APPLICATION_CREDENTIALS=CI_RUN_ID.json
 gcloud config set project red-inspr
 
 gsutil rsync bin gs://inspr-cli/$VERSION
--- .github/scripts/releasecli.sh
+++ .github/scripts/releasecli.sh
@@ -1,9 +1,10 @@
+GITHUB_SHA=$1
 VERSION=$(cat version)
 BASE_URL="https://console.cloud.google.com/storage/browser/inspr-cli/bin"
 
 release-cli create --name "Release $VERSION" \
         --tag-name $VERSION \
-        --ref "${{ github.sha }}" \
+        --ref GITHUB_SHA \
         --assets-link "{\"name\":\"insprcli-linux-arm64\",
                         \"url\":\"$BASE_URL/insprcli-linux-arm64-$VERSION\"}" \
         --assets-link "{\"name\":\"insprcli-linux-amd64\",
