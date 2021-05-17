a2ae5156144692aa39f2a1ac9d797a479cb053ee
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2021-05-17 11:53:59 -0300

    dev (mem - brokers) changed interfaces of broker managment.

--- cmd/insprd/memory/brokers/interface.go
+++ cmd/insprd/memory/brokers/interface.go
@@ -1,16 +1,29 @@
 package brokers
 
-import "github.com/inspr/inspr/pkg/meta/brokers"
+import (
+	"github.com/inspr/inspr/pkg/meta/brokers"
+	"github.com/inspr/inspr/pkg/sidecar/models"
+)
 
+// Manager is the interface that allows the management
+// of the system's message brokers.
 type Manager interface {
 	Brokers() BrokerInterface
 }
 
-// BrokerInterface is the interface tht allows for interaction
-// with the systems multiple brokers
+// BrokerInterface is the interface that allows for interaction
+// with the systems multiple brokers and its conficurations.
 type BrokerInterface interface {
 	GetAll() brokers.BrokerStatusArray
 	GetDefault() brokers.BrokerStatus
 	Create(broker brokers.BrokerStatus, config brokers.BrokerConfiguration) error
 	SetDefault(broker brokers.BrokerStatus) error
+	Factory() SidecarInterface
+}
+
+// SidecarInterface is the interface that allows the build and deployment of
+// available brokers
+type SidecarInterface interface {
+	Get(broker string) models.SidecarFactory
+	Subscribe(broker string, factory models.SidecarFactory) error
 }
