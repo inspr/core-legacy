461f2fe0989e1eb3f392e39e278fd5531195987b
Francisco Andrade <francisco@inspr.com>
2021-05-18 13:44:07 -0300

    fix(sidecar server): removed unused test structures

--- pkg/sidecar/server/handlers_test.go
+++ pkg/sidecar/server/handlers_test.go
@@ -17,30 +17,6 @@ import (
 	"github.com/inspr/inspr/pkg/sidecar/models"
 )
 
-// sendInRequest is a struct used for all the testing files in this package
-// it's contents is a simple { body []byte }
-type sendInRequest struct{ body []byte }
-
-// expectedResponse is a struct used for all the testing files in this package
-// it's contents is a simple { status int }
-type wantedResponse struct{ status int }
-
-// args is a struct to setup the values to be sent and the ones
-// that are expected in the request
-type args struct {
-	send sendInRequest
-	want wantedResponse
-}
-
-// testCaseStruct stores the complete struct used in testing
-// containing all the structs defined in the testing file
-// sets the name of the test, it's handler and it's args
-type testCaseStruct struct {
-	name string
-	ch   *Server
-	args args
-}
-
 // createMockEnvVars - sets up the env values to be used in the tests functions
 func createMockEnvVars() {
 	customEnvValues := "chan;testing;banana"
@@ -85,8 +61,6 @@ func (m mockWriter) WriteMessage(channel string, message interface{}) error {
 func TestServer_writeMessageHandler(t *testing.T) {
 	createMockEnvVars()
 	defer deleteMockEnvVars()
-	type fields struct {
-	}
 	tests := []struct {
 		readerFunc func(t *testing.T) mockReader
 		writerFunc func(t *testing.T) mockWriter
@@ -283,7 +257,8 @@ func TestServer_readMessageRoutine(t *testing.T) {
 				if received == tt.wantErr {
 					t.Errorf("Server_readMessageRoutine received = %v, wantErr = %v", received, tt.wantErr)
 				}
-
+			default:
+				return
 			}
 		})
 	}
