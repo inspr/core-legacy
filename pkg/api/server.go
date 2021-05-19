44189118caebd595768f52f221e58a2a9de78781
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2021-05-19 09:21:54 -0300

    dev (pkg - aoi): api's structures broker manager insertion

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"github.com/inspr/inspr/cmd/insprd/memory"
+	"github.com/inspr/inspr/cmd/insprd/memory/brokers"
 	"github.com/inspr/inspr/cmd/insprd/operators"
 	ctrl "github.com/inspr/inspr/pkg/api/controllers"
 	"github.com/inspr/inspr/pkg/auth"
@@ -10,8 +11,8 @@ import (
 var server ctrl.Server
 
 // Run is the server start up function
-func Run(mm memory.Manager, op operators.OperatorInterface, auth auth.Auth) {
+func Run(mm memory.Manager, op operators.OperatorInterface, auth auth.Auth, bm brokers.Manager) {
 	// server.Init(mocks.MockMemoryManager(nil))
-	server.Init(mm, op, auth)
+	server.Init(mm, op, auth, bm)
 	server.Run(":8080")
 }
