8e18f1de004b640f6220f61c7caf602388c09dff
LucasStarlingdePaulaSalles <lucastarling1@gmail.com>
2021-05-20 14:25:46 -0300

    dev (cli): adapted cluster command to inspr's cli pattern

---
+++ cmd/inspr/cli/cluster.go
@@ -0,0 +1,61 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/inspr/inspr/pkg/cmd"
+	"github.com/inspr/inspr/pkg/cmd/utils"
+	"github.com/spf13/cobra"
+)
+
+func NewClusterCommand() *cobra.Command {
+	getBrokers := cmd.NewCmd("brokers").
+		WithDescription("Retrieves brokers currently installed").
+		WithAliases("b").
+		NoArgs(getBrokers)
+	auth_init := cmd.NewCmd("init").
+		WithDescription("Init configures insprd's default token").
+		WithCommonFlags().
+		ExactArgs(1, authInit)
+	return cmd.NewCmd("cluster").
+		WithDescription("Configure aspects of your inspr cluster").
+		WithLongDescription("Cluster takes a subcommand of (brokers | init)").
+		WithExample("get cluster's brokers", "inspr cluster brokers").
+		WithExample("init insprd as admin", " inspr cluster init <admin_name> <admin_password>").
+		AddSubCommand(getBrokers, auth_init).
+		Super()
+}
+
+func getBrokers(ctx context.Context) error {
+	client := utils.GetCliClient()
+	out := utils.GetCliOutput()
+	resp, err := client.Brokers().Get(context.Background())
+	if err != nil {
+		utils.RequestErrorMessage(err, out)
+		return err
+	}
+
+	fmt.Fprintf(out, "DEFAULT:\n%s\n", resp.Default)
+	fmt.Fprintln(out, "AVAILABLE:")
+	lines := make([]string, 0)
+	for _, broker := range resp.Installed {
+		printLine(broker, &lines)
+	}
+	printTab(&lines)
+	return nil
+}
+
+func authInit(c context.Context, args []string) error {
+	output := utils.GetCliOutput()
+
+	token, err := utils.GetCliClient().Authorization().Init(c, args[0])
+	if err != nil {
+		utils.RequestErrorMessage(err, output)
+		return err
+	}
+
+	fmt.Fprintln(output, "This is a root token for authentication within your insprd. This will not be generated again. Save it wisely.")
+	fmt.Fprintf(output, "%s\n", token)
+	return nil
+}
