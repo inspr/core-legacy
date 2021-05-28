20ba37a6866a5a5d95a420a8b35d547bcf764f7c
Francisco Andrade <francisco@inspr.com>
2021-05-28 12:47:06 -0300

    test(examples): tested controller example

--- examples/primes/yamls/pingpong.go
+++ examples/primes/yamls/pingpong.go
@@ -3,5 +3,6 @@ package yamls
 import _ "embed" // needs to be imported for embed to work
 
 //go:embed general.yaml
-// PingPongYAML is the yaml of the primes example
-var PingPongYAML string
+
+// PrimesYAML is the yaml of the primes example
+var PrimesYAML string
