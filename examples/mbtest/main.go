32a374d47289b07c442bf95af5d320b844217cb2
Lucas <lucas@inspr.com>
2021-06-03 12:45:53 -0300

    test (MB Integration): succesfully ran code using multi-broker arquitechture

--- examples/mbtest/main.go
+++ examples/mbtest/main.go
@@ -2,24 +2,56 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"time"
 
 	dappclient "github.com/inspr/inspr/pkg/client"
+	"github.com/inspr/inspr/pkg/sidecars/models"
 )
 
 func main() {
-	counter := 1000
+	counter := 5
+	i := 0
 	// sets up client for sidecar
 	c := dappclient.NewAppClient()
 
+	ctx, cancel := context.WithCancel(context.Background())
+
 	message := 1234
 
-	for i := 0; i < counter; i++ {
-		err := c.WriteMessage(context.Background(), "mbch1", message)
+	c.HandleChannel("mbch1", func(ctx context.Context, body io.Reader) error {
+		decoder := json.NewDecoder(body)
+		var testMsg models.BrokerMessage
+		err := decoder.Decode(&testMsg)
+		if err != nil {
+			return err
+		}
+		i++
+		fmt.Println(testMsg)
+		if i >= counter {
+			cancel()
+			return nil
+		}
+		time.Sleep(time.Second * 10)
+		err = c.WriteMessage(context.Background(), "mbch1", message)
 		if err != nil {
 			fmt.Printf("an error occurred: %v", err)
+			return err
 		}
-		time.Sleep(time.Second * 5)
+		return nil
+	})
+
+	go c.Run(ctx)
+
+	err := c.WriteMessage(context.Background(), "mbch1", message)
+	if err != nil {
+		fmt.Printf("an error occurred: %v", err)
+		return
 	}
+
+	<-ctx.Done()
+	fmt.Println("Done w/ exec")
+	fmt.Scan()
 }
