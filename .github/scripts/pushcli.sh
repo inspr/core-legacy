55eebfeffb2f88a352295eb4fab5f4a052b1cddb
Francisco Andrade <francisco@inspr.com>
2021-06-21 09:36:47 -0300

    fix(devops): removed unnecessary config from pushcli.sh

--- .github/scripts/pushcli.sh
+++ .github/scripts/pushcli.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-GCR_DETAILS=$1
-CI_RUN_ID=$2
 VERSION=$(cat version)
-
-gcloud auth configure-docker
-echo $GCR_DETAILS > ($CI_RUN_ID).json
-gcloud auth activate-service-account --key-file ($CI_RUN_ID).json
-export GOOGLE_APPLICATION_CREDENTIALS=($CI_RUN_ID).json
-gcloud config set project red-inspr
 
 gsutil rsync bin gs://inspr-cli/$VERSION
 
