99b5650e61d64d89bb1d962adcaf17597b83f74e
Francisco Andrade <francisco@inspr.com>
2021-06-28 11:54:33 -0300

    misc(examples): fixed lint for exported var in '/primes'

--- examples/primes/yamls/primes.go
+++ examples/primes/yamls/primes.go
@@ -3,4 +3,5 @@ package yamls
 import _ "embed" // needs to be imported for embed to work
 
 //go:embed general.yaml
-var PrimesYAML string // PrimesYAML is the yaml of the primes example
+// PrimesYAML is the yaml of the primes example
+var PrimesYAML string
