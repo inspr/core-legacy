56c7ccbc15215b6c31b0494e07d3417123769817
Pedro Tavares de Carvalho <pcarvalho@cyral.com>
2021-07-07 13:01:38 -0300

    dev(devops): update helm update scripts

--- .github/scripts/update_tags.sh
+++ .github/scripts/update_tags.sh
@@ -2,23 +2,17 @@
 CHART_URL=$1
 CHART_GS_URI=$2
 echo "Updating helm chart tags"
-INSPRD_TAG=$(printf '%s' $(grep -Eo "gcr\.io\/insprlabs\/insprd\:[^@]+\@sha256\:[[:alnum:]]+" tags.out) | sed -e 's/[\/&]/\\&/g')
-LB_SIDECAR_TAG=$(printf '%s' $(grep -Eo "gcr\.io\/insprlabs\/inspr\/sidecar\/lbsidecar\:[^@]+\@sha256\:[[:alnum:]]+" tags.out) | sed -e 's/[\/&]/\\&/g')
-AUTHSVC_TAG=$(printf '%s' $(grep -Eo "gcr\.io\/insprlabs\/authsvc\:[^@]+\@sha256\:[[:alnum:]]+" tags.out) | sed -e 's/[\/&]/\\&/g')
-SECRETGEN_TAG=$(printf '%s' $(grep -Eo "gcr\.io\/insprlabs\/secretgen\:[^@]+\@sha256\:[[:alnum:]]+" tags.out) | sed -e 's/[\/&]/\\&/g')
 
-echo
-echo "Tags for updating:"
-echo "$INSPRD_TAG"
-echo "$LB_SIDECAR_TAG"
-echo "$AUTHSVC_TAG"
-echo "$SECRETGEN_TAG"
+OUTPUT=$(cat tags.out | jq ".builds[]" --compact-output)
+for k in ${OUTPUT[@]}; do
+    IMAGE=`printf %s $(echo $k | jq .imageName)`
+    IMAGE=$(eval echo $IMAGE)
+    echo "Setting $IMAGE tag to : $TAG"
+    TAG=`printf %s $(echo $k | jq .tag)`
+    TAG=$(eval echo $TAG)
+    sed -i s_${IMAGE}_${TAG}_g ./build/helm/values.yaml
+done
 
-echo "Updating tags"
-sed -i 's/gcr\.io\/insprlabs\/insprd/'"$INSPRD_TAG"'/'  build/helm/values.yaml
-sed -i 's/gcr\.io\/insprlabs\/inspr\/sidecar\/lbsidecar/'"$LB_SIDECAR_TAG"'/' build/helm/values.yaml
-sed -i 's/gcr\.io\/insprlabs\/authsvc/'"$AUTHSVC_TAG"'/'  build/helm/values.yaml
-sed -i 's/gcr\.io\/insprlabs\/secretgen/'"$SECRETGEN_TAG"'/'  build/helm/values.yaml
 
 APP_VERSION=$(git describe --always --tags)
 echo "Updating chart app version to $APP_VERSION"
@@ -33,7 +27,7 @@ sed -i 's/version: .*/version: '"$NEW_VERSION"'/' build/helm/Chart.yaml
 
 
 echo "Updating helm dependencies..."
-helm dependency  update build/helm
+helm dependency update build/helm
 
 echo "Generating new helm charts"
 helm package build/helm -d charts
