86ed60f4f9fd28b680425aabe39411fad1214df5
Pedro Tavares de Carvalho <pcarvalho@cyral.com>
2021-08-24 19:47:11 -0300

    dev(secret): changes secret generator to not generate the password

--- cmd/uid_provider/secret/main.go
+++ cmd/uid_provider/secret/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"context"
-	"crypto/rand"
-	"encoding/hex"
 	"log"
 	"os"
 
@@ -11,23 +9,8 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"strings"
-	"math/big"
 )
 
-func generatePassword() string {
-	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-		"abcdefghijklmnopqrstuvwxyz" +
-		"0123456789")
-	length := 20
-	var b strings.Builder
-	for i := 0; i < length; i++ {
-		index, _ := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
-		b.WriteRune(chars[index.Int64()])
-	}
-	str := b.String()
-	return str
-}
 var clientSet kubernetes.Interface
 
 func initInsprd() ( string, error ){
@@ -59,36 +42,24 @@ func main() {
 	namespace := os.Getenv("K8S_NAMESPACE")
 	secretName := os.Getenv("SECRET_NAME")
 
-
 	initKube()
 	secret, err := clientSet.CoreV1().Secrets(namespace).Get(ctx,secretName, v1.GetOptions{})
 	if err != nil {
 		panic(err)
 	}
 
-	if _, exists := secret.Data["REFRESH_KEY"]; !exists {
-		bytes := make([]byte, 32) //generate a random 32 byte key for AES-256
-		if _, err := rand.Read(bytes); err != nil {
-			panic(err.Error())
-		}
+	if secret.Data == nil {
+		secret.Data = make(map[string][]byte)
+	}
 
-		key := hex.EncodeToString(bytes)
-		privateKeyBytes := []byte(key)
-		secret.Data["REFRESH_KEY"] = privateKeyBytes
-		if os.Getenv("INIT_INSPRD") == "true" {
-			token, err := initInsprd()
-			if err != nil {
-				panic(err)
-			}
-			secret.Data["ADMIN_TOKEN"] = []byte(token)
-		}
-		if os.Getenv("ADMIN_PASSWORD_GENERATE") == "true" {
-			secret.Data["ADMIN_PASSWORD"] = []byte(generatePassword())	
-		}
+	token, err := initInsprd()
+	if err != nil {
+		panic(err)
+	}
+	secret.Data["ADMIN_TOKEN"] = []byte(token)
 
-		_, err = clientSet.CoreV1().Secrets(namespace).Update(ctx, secret, v1.UpdateOptions{})
-		if err != nil {
-			log.Fatal(err.Error())
-		}
+	_, err = clientSet.CoreV1().Secrets(namespace).Update(ctx, secret, v1.UpdateOptions{})
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 }
