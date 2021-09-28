a98f6b03fd6d0d9e9680593a958e891b958aa650
Lucas <lucas@inspr.com>
2021-09-28 12:39:13 -0300

    dev (mem-dapp): route resolution improved to include read port

--- cmd/insprd/operators/nodes/converter.go
+++ cmd/insprd/operators/nodes/converter.go
@@ -151,7 +151,7 @@ func (no *NodeOperator) withRoutes(app *meta.App) k8s.ContainerOption {
 	return func(c *corev1.Container) {
 		env := make(utils.EnvironmentMap)
 		for route, data := range app.Spec.Routes {
-			raw := "node-" + data.Address + ";"
+			raw := data.Address + ";"
 			raw = raw + data.Endpoints.Join(";")
 			env[route+"_ROUTE"] = raw
 		}
