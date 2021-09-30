ad32b26f62eca956838783bd71c4353a9494041d
luiz-couto <luiz.felipe.556@outlook.com>
2021-09-30 12:57:46 -0300

    dev(client):  update logs

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -77,7 +77,7 @@ func (c *Client) WriteMessage(ctx context.Context, channel string, msg interface
 // HandleChannel handles messages received in a given channel.
 func (c *Client) HandleChannel(channel string, handler func(ctx context.Context, body io.Reader) error) {
 	c.mux.HandleFunc("/channel/"+channel, func(w http.ResponseWriter, r *http.Request) {
-		logger.Info("received request on client handle channel", zap.String("Channel", channel))
+		logger.Info("received request on client handle channel", zap.String("channel", channel))
 		// user defined handler. Returns error if the user wants to return it
 		err := handler(context.Background(), r.Body)
 		if err != nil {
@@ -93,7 +93,7 @@ func (c *Client) HandleChannel(channel string, handler func(ctx context.Context,
 func (c *Client) HandleRoute(path string, handler func(w http.ResponseWriter, r *http.Request)) {
 	path = strings.TrimPrefix(path, "/")
 	c.mux.HandleFunc("/route/"+path, func(w http.ResponseWriter, r *http.Request) {
-		logger.Info("received request on client handle route", zap.String("Route", path))
+		logger.Info("received request on client handle route", zap.String("route", path))
 		handler(w, r)
 	})
 }
