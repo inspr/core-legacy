fe4aae8ce482a0288cb7502f95343b62ba9c23fd
luiz-couto <luiz.felipe.556@outlook.com>
2021-10-08 09:22:21 -0300

    dev(python route demo): now not terminating when error on client

--- examples/python/route_demo/client/client.py
+++ examples/python/route_demo/client/client.py
@@ -11,8 +11,8 @@ API_NAME = "api"
 def main():
     client = inspr.Client()
     msg_body = {
-        "op1": 1,
-        "op2": 2,
+        "op1": 5,
+        "op2": 3,
     }
 
     print("Python Client")
@@ -20,12 +20,32 @@ def main():
     while True:
         try:
             resp = client.send_request(API_NAME, ADD_PATH, method="POST", body=msg_body)
+            res = resp.json()
+            print("The result of 5 + 3 = ", res)
         except:
             print("An error has occured", file=sys.stderr)
-            return
+        
+        time.sleep(3)
 
-        print("The result is ", resp.json())
-        time.sleep(10)
+        try:
+            resp = client.send_request(API_NAME, SUB_PATH, method="POST", body=msg_body)
+            res = resp.json()
+            print("The result of 5 - 3 = ", res)
+        except:
+            print("An error has occured", file=sys.stderr)
+        
+        time.sleep(3)
+
+        try:
+            resp = client.send_request(API_NAME, MUL_PATH, method="POST", body=msg_body)
+            res = resp.json()
+            print("The result of 5 * 3 = ", res)
+        except:
+            print("An error has occured", file=sys.stderr)
+        
+        time.sleep(3)
+
+
 
 
 if __name__ == "__main__":
