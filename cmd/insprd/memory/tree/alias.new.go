8b24ee14467f5d6c7fc597ee9512a38248de13d8
luiz-couto <luiz.felipe.556@outlook.com>
2022-01-18 10:03:02 -0300

    (dev): new alias api update

---
+++ cmd/insprd/memory/tree/alias.new.go
@@ -0,0 +1,189 @@
+package tree
+
+import (
+	"go.uber.org/zap"
+	"inspr.dev/inspr/pkg/ierrors"
+	"inspr.dev/inspr/pkg/meta"
+	"inspr.dev/inspr/pkg/meta/utils"
+)
+
+// AliasMemoryManager implements the Alias interface
+// and provides methods for operating on Aliases
+type AliasMemoryManagerNew struct {
+	*treeMemoryManager
+	logger *zap.Logger
+}
+
+// Alias is a MemoryManager method that provides an access point for Alias
+func (tmm *treeMemoryManager) AliasNew() AliasMemory {
+	return &AliasMemoryManager{
+		treeMemoryManager: tmm,
+		logger:            logger.With(zap.String("subSection", "alias")),
+	}
+}
+
+// Get receives a scope and an alias key. The scope defines
+// the path to a dApp. If this dApp has a pointer to a alias that has the
+// same key as the key passed as an argument, the pointer to that alias is returned
+func (amm *AliasMemoryManagerNew) GetNew(scope, name string) (*meta.Alias, error) {
+	l := amm.logger.With(
+		zap.String("operation", "get"),
+		zap.String("alias", name),
+		zap.String("scope", scope),
+	)
+	l.Debug("trying to get an Alias")
+
+	app, err := amm.treeMemoryManager.Apps().Get(scope)
+	if err != nil {
+		l.Debug("unable to get Alias because the app was not found")
+		return nil, err
+	}
+
+	if _, ok := app.Spec.Aliases[name]; !ok {
+		l.Debug("alias not found with the given name")
+		return nil, ierrors.New(
+			"alias not found with the given name %v", name,
+		).NotFound()
+	}
+
+	return app.Spec.Aliases[name], nil
+}
+
+// Create receives a scope that defines a path to the dapp and an Alias
+// to be added in this dapp
+func (amm *AliasMemoryManagerNew) Create(scope string, alias *meta.Alias) error {
+	l := amm.logger.With(
+		zap.String("operation", "create"),
+		zap.Any("alias", alias),
+		zap.String("scope", scope),
+	)
+	l.Debug("trying to create an Alias")
+
+	app, err := amm.Apps().Get(scope)
+	if err != nil {
+		l.Debug("unable to create Alias because the app was not found")
+		return err
+	}
+
+	if _, ok := app.Spec.Aliases[alias.Meta.Name]; ok {
+		l.Debug("alias already exists")
+		return ierrors.New("alias already exists in dApp").AlreadyExists()
+	}
+
+	err = amm.checkSource(scope, app, alias)
+	if err != nil {
+		return err
+	}
+
+	err = amm.checkDestination(app, alias)
+	if err != nil {
+		return err
+	}
+
+	alias.Meta = utils.InjectUUID(alias.Meta)
+	l.Debug("adding Alias to dApp",
+		zap.String("dApp", app.Meta.Name))
+	app.Spec.Aliases[alias.Meta.Name] = alias
+
+	return nil
+}
+
+// Update receives a scope and a alias object. The scope
+// defines the path to the dApp that contains the Alias. If the dApp has
+// an alias that has the same name as the one passed as an argument,
+// that alias will be replaced by the new alias
+func (amm *AliasMemoryManagerNew) Update(scope string, alias *meta.Alias) error {
+	l := amm.logger.With(
+		zap.String("operation", "update"),
+		zap.Any("alias", alias),
+		zap.String("scope", scope),
+	)
+	l.Debug("trying to update an Alias")
+
+	app, err := amm.treeMemoryManager.Apps().Get(scope)
+	if err != nil {
+		l.Debug("unable to update Alias because the app was not found")
+		return err
+	}
+
+	selectedAlias, ok := app.Spec.Aliases[alias.Meta.Name]
+	if !ok {
+		l.Debug("alias was not found")
+		return ierrors.New("alias was not found in dApp").NotFound()
+	}
+
+	err = amm.checkSource(scope, app, alias)
+	if err != nil {
+		return err
+	}
+
+	err = amm.checkDestination(app, alias)
+	if err != nil {
+		return err
+	}
+
+	alias.Meta.UUID = selectedAlias.Meta.UUID
+	l.Debug("replacing old Alias with the new one",
+		zap.String("dapp", app.Meta.Name))
+	app.Spec.Aliases[alias.Meta.Name] = alias
+
+	return nil
+
+}
+
+func (amm *AliasMemoryManagerNew) Delete(scope, name string) error {
+	l := amm.logger.With(
+		zap.String("operation", "delete"),
+		zap.Any("alias", name),
+		zap.String("scope", scope),
+	)
+	l.Debug("trying to delete an Alias")
+
+	app, err := amm.treeMemoryManager.Apps().Get(scope)
+	if err != nil {
+		return ierrors.Wrap(err, "cannot delete alias because the dapp was not found")
+	}
+
+	if _, ok := app.Spec.Aliases[name]; !ok {
+		return ierrors.New(
+			"alias not found with the given name %v", name,
+		).NotFound()
+	}
+
+	return nil
+}
+
+func (amm *AliasMemoryManagerNew) checkSource(scope string, app *meta.App, alias *meta.Alias) error {
+	var source *meta.App
+	if alias.Source == "" {
+		parentApp, err := getParentApp(scope, amm.treeMemoryManager)
+		if err != nil {
+			return ierrors.Wrap(err, "cannot find parent dapp")
+		}
+		source = parentApp
+	} else {
+		childApp, ok := app.Spec.Apps[alias.Source]
+		if !ok {
+			return ierrors.New("cannot find source child dapp with the name '%v'", alias.Source).NotFound()
+		}
+		source = childApp
+	}
+
+	_, hasChannel := source.Spec.Channels[alias.Resource]
+	_, hasRoute := source.Spec.Routes[alias.Resource]
+	if !hasChannel && !hasRoute {
+		return ierrors.New("cannot find resource with the name '%v'", alias.Resource).NotFound()
+	}
+
+	return nil
+}
+
+func (amm *AliasMemoryManagerNew) checkDestination(app *meta.App, alias *meta.Alias) error {
+	if alias.Destination != "" {
+		_, ok := app.Spec.Apps[alias.Destination]
+		if !ok {
+			return ierrors.New("cannot find destination child dapp with the name '%v'", alias.Destination).NotFound()
+		}
+	}
+	return nil
+}
