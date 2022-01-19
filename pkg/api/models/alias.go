2a22ac1a08689d91e1de12c99c60a93fdf1f07ba
luiz-couto <luiz.felipe.556@outlook.com>
2022-01-19 10:51:02 -0300

    dev(alias handler): support for new architecture

--- pkg/api/models/alias.go
+++ pkg/api/models/alias.go
@@ -5,12 +5,11 @@ import "inspr.dev/inspr/pkg/meta"
 // AliasDI - Data Input(DI) format for requests that pass the alias data
 type AliasDI struct {
 	Alias  meta.Alias `json:"alias"`
-	Target string     `json:"target"`
 	DryRun bool       `json:"dry"`
 }
 
 // AliasQueryDI - Data Input format for queries requests
 type AliasQueryDI struct {
-	Key    string `json:"key"`
+	Name   string `json:"name"`
 	DryRun bool   `json:"dry"`
 }
