206afcf6ce511a140202a618485ebfafa2a9d379
Lucas <lucas@inspr.com>
2022-02-04 12:41:44 -0300

    fix (pkg - sidecars): removed kafka lib conflict

--- pkg/sidecars/lbsidecar/handlers_test.go
+++ pkg/sidecars/lbsidecar/handlers_test.go
@@ -15,7 +15,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 	"inspr.dev/inspr/pkg/rest"
 	"inspr.dev/inspr/pkg/sidecars/models"
 )
